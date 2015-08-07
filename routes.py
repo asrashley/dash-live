22b849bf4d2e45a618d1ba60a3ca47be04876ec4
asrashley <alex@ashley-family.net>
2015-08-07 12:49:42 +0100

    changed double quote to single quote

--- routes.py
+++ routes.py
@@ -8,12 +8,12 @@ class Route(object):
         self.parent=parent
 
 routes = {
-    "dash-mpd":Route(r'/live/<manifest:[\w\-_]+\.mpd>', handler='views.LiveManifest', title="Live test stream"),
-    "dash-media":Route(r'/dash/<filename:(A1|V[1-4])>/<segment:(\d+|init)>.<ext:(mp4|m4v|m4a|m4s)>', handler='views.LiveMedia', title="DASH fragment"),
-    "video":Route(r'/video', handler='views.VideoPlayer', title="DASH test stream"),
-    "home":Route(r'/', handler='views.MainPage', title="DASH test streams"),
+    "dash-mpd":Route(r'/dash/<manifest:[\w\-_]+\.mpd>', handler='views.LiveManifest', title='DASH test stream'),
+    "dash-media":Route(r'/dash/<filename:(A1|V[1-3])>/<segment:(\d+|init)>.<ext:(mp4|m4v|m4a|m4s)>', handler='views.LiveMedia', title="DASH fragment"),
+    "video":Route(r'/video', handler='views.VideoPlayer', title='DASH test stream player'),
+    "home":Route(r'/', handler='views.MainPage', title='DASH test streams'),
     "uploadBlob":Route(r'/blob', handler='views.UploadHandler', title='Upload blob'),
-    "upload":Route(r'/upload', handler='views.UploadFormHandler', title="Upload media file"),
+    "upload":Route(r'/upload', handler='views.UploadHandler', title='Upload media file'),
 }
 
 for name,r in routes.iteritems():
