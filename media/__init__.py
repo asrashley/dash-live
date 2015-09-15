a5eaccd5554b9c3234f5d3cc7a877edc2f16d191
asrashley <alex@ashley-family.net>
2015-09-15 07:44:28 +0100

    add an additional E-AC3 audio representation
    To allow testing of combinations of AAC and E-AC3 audio, add
    another audio track that has 5.1 channel audio and uses the E-AC3
    codec. The AAC track is configured to have the main role and the
    E-AC3 track is configurated to have the alternate role.

--- media/__init__.py
+++ media/__init__.py
@@ -1,6 +1,6 @@
 #
 
-import V1,V2,V3,A1, V3ENC
+import V1, V2, V3, A1, A2, V3ENC
 import os
 from segment import Representation, Segment
 
@@ -8,5 +8,6 @@ representations = { 'V1':V1.representation,
                     'V2':V2.representation, 
                     'V3':V3.representation,
                     'V3ENC':V3ENC.representation,
-                    'A1':A1.representation
+                    'A1':A1.representation,
+                    'A2':A2.representation
                     }
