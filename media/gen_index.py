3990451a70dbddca2a992a61a74ef73d82cc9cab
asrashley <alex@ashley-family.net>
2015-09-21 14:52:48 +0100

    add option to split MP4 file into fragments
    Add a --split option that allows an MP4 file to be split into its
    individual fragments.

--- media/gen_index.py
+++ media/gen_index.py
@@ -812,30 +812,50 @@ def create_index_file(filename, args):
         dest.write('mediaPresentationDuration="%s" minBufferTime="PT10S" '%utils.toIsoDuration(repr.media_duration/repr.timescale))
         dest.write('profiles="urn:mpeg:dash:profile:isoff-on-demand:2011" type="static" ')
         dest.write('xsi:schemaLocation="urn:mpeg:dash:schema:mpd:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd">\n')
-        dest.write(' <Period>\n')
-        if hasattr(repr, 'frameRate'):
-            mimeType='video/mp4'
-            contentType="video"
-        else:
+        dest.write(' <Period start="PT0S" duration="%s">\n'%utils.toIsoDuration(repr.media_duration/repr.timescale))
+        if repr.contentType=='audio':
+            ext = 'm4a'
             mimeType='audio/mp4'
-            contentType="audio"
-        dest.write('  <AdaptationSet contentType="%s" group="1" lang="%s" mimeType="%s" segmentAlignment="true" subsegmentAlignment="true" subsegmentStartsWithSAP="1">\n'%(contentType,repr.language,mimeType))
+        else:
+            ext = 'm4v'
+            mimeType='video/mp4'
+        dest.write('  <AdaptationSet contentType="%s" group="1" lang="%s" mimeType="%s" segmentAlignment="true" subsegmentAlignment="true" subsegmentStartsWithSAP="1">\n'%(repr.contentType,repr.language,mimeType))
         try:
             dest.write('   <Representation audioSamplingRate="%d" bandwidth="%d" codecs="%s" id="%s">\n'%(repr.sampleRate, repr.bitrate, repr.codecs, repr.id))
         except AttributeError:
             dest.write('   <Representation frameRate="%d" bandwidth="%d" codecs="%s" id="%s" height="%d" width="%d">\n'%(repr.frameRate, repr.bitrate, repr.codecs, repr.id, repr.height, repr.width))
-        dest.write('     <BaseURL>%s</BaseURL>\n'%filename)
-        dest.write('     <SegmentList duration="%d" timescale="%d">\n'%(repr.media_duration, repr.timescale))
-        dest.write('       <Initialization range="%d-%d"/>\n'%(repr.segments[0].seg.pos,repr.segments[0].seg.pos+repr.segments[0].seg.size-1))
-        for seg in repr.segments[1:]:
-            dest.write('       <SegmentURL d="%d" mediaRange="%d-%d"/>\n'%(seg.seg.duration, seg.seg.pos, seg.seg.pos+seg.seg.size-1))
-        dest.write('     </SegmentList>\n')
+        if args.split:
+            dest.write('     <SegmentTemplate presentationTimeOffset="0" timescale="%d" startNumber="1" '%(repr.timescale))
+            dest.write('initialization="$RepresentationID$/init.%s" media="$RepresentationID$/$Number%%05d$.%s" '%(ext,ext))
+            dest.write('duration="%d"/>\n'%(repr.segment_duration))
+        else:
+            dest.write('     <BaseURL>%s</BaseURL>\n'%filename)
+            dest.write('     <SegmentList duration="%d" timescale="%d">\n'%(repr.segment_duration, repr.timescale))
+            dest.write('       <Initialization range="%d-%d"/>\n'%(repr.segments[0].seg.pos,repr.segments[0].seg.pos+repr.segments[0].seg.size-1))
+            for seg in repr.segments[1:]:
+                dest.write('       <SegmentURL d="%d" mediaRange="%d-%d"/>\n'%(seg.seg.duration, seg.seg.pos, seg.seg.pos+seg.seg.size-1))
+            dest.write('     </SegmentList>\n')
         dest.write('   </Representation>\n')
         dest.write('  </AdaptationSet>\n')
         dest.write(' </Period>\n')
         dest.write('</MPD>\n')
         dest.close()
-    else:
+    if args.split:
+        ext = 'm4a' if repr.contentType=='audio' else 'm4v'
+        src_file = io.FileIO(filename, 'rb')
+        for idx,seg in enumerate(repr.segments):
+            if idx==0:
+                dst_filename = os.path.join(args.split[0],'init.%s'%ext)
+            else:
+                dst_filename = os.path.join(args.split[0], '%05d.%s'%(idx,ext))
+            print dst_filename
+            dst_file = io.FileIO(dst_filename,'wb')
+            src_file.seek(seg.seg.pos)
+            data = src_file.read(seg.seg.size)
+            dst_file.write(data)
+            dst_file.close()
+        src_file.close()
+    if args.index:
         print('Creating '+repr.id+'.py')
         dest = open(repr.id+'.py', 'wb')
         dest.write('from segment import Representation\n')
@@ -847,7 +867,9 @@ def create_index_file(filename, args):
 parser = argparse.ArgumentParser(description='MP4 parser and index generation')
 parser.add_argument('-d', '--debug', action="store_true")
 parser.add_argument('-c', '--codec', help='MP4 file that contains codec information', nargs=1, metavar=('mp4file'))
+parser.add_argument('-i', '--index', help='Generate a python index file', action="store_true")
 parser.add_argument('-m', '--manifest', help='Generate a manifest file', nargs=1, metavar=('mpdfile'))
+parser.add_argument('-s', '--split', help='Split MP4 file into fragments', nargs=1, metavar=('mp4file'))
 parser.add_argument('mp4file', help='Filename of MP4 file', nargs='+', default=None)
 args = parser.parse_args()
 
