asrashley/dash-live

commit 2a324ef6afd83c39d462029d36a597f4e23ca0a3

change video page to make use of testcases module
Rather than try and pass all the parameters for the video page via
CGI parameters, change the routing to specify the ID of a test case
and then lookup the testcase using this ID.

This allows a simple (persistent) URL to be used for each test case.

static/js/main.js
@@ -1,47 +1,45 @@
 $(document).ready(function(){
-	'use strict';
-	function changePage(anchor){
-		var $a = $(anchor);
-		var title = $(anchor).parents('tr').find('th.title > .title').text();
-		document.location.assign('/video?url='+encodeURI($a.attr('href'))+'&title='+encodeURI(title));
-	}
-	
-	$('body').on('keydown', function(ev){
-		console.dir(ev);
-		var btn=null;
-		
-		if(ev.keyCode>=48 && ev.keyCode<=57){
-			btn = $('#btn'+String.fromCharCode(ev.keyCode));			
-			switch(ev.keyCode){
-			case 48: // 0
-				if(btn && btn.length && btn.hasClass('prev')){
-					document.location.assign(btn.find('a').attr('href'));
-					return;
-				}
-				break;			
-			case 57: // 9
-				if(btn && btn.length && btn.hasClass('next')){
-					document.location.assign(btn.find('a').attr('href'));
-					return;
-				}
-				break;
-			}
-			if(btn && btn.length){
-				changePage(btn.find('a')[0]);
-			}
-		}
-	});
-	$('.btn-grid a').on('click', function(ev){
-		if(ev.target.classList.hasClass('prev') || ev.target.classList.hasClass('prev')){
-			/* follow the link for prev/next page links */
-			return(true);
-		}
-		changePage(ev.target);
-		ev.preventDefault();
-		return(false);
-	});
-	/*$('.btn p').on('mouseenter', function(ev){
-		
-	}); */
-	$('.tooltip').tooltipster();
+  'use strict';
+  function changePage(anchor){
+    var $a = $(anchor);
+    document.location.assign('/video/'+$a.data('testcase'));
+  }
+
+  $('body').on('keydown', function(ev){
+    var btn=null;
+
+    if(ev.keyCode>=48 && ev.keyCode<=57){
+      btn = $('#btn'+String.fromCharCode(ev.keyCode));
+      switch(ev.keyCode){
+      case 48: // 0
+        if(btn && btn.length && btn.hasClass('prev')){
+          document.location.assign(btn.find('a').attr('href'));
+          return;
+        }
+        break;
+      case 57: // 9
+        if(btn && btn.length && btn.hasClass('next')){
+          document.location.assign(btn.find('a').attr('href'));
+          return;
+        }
+        break;
+      }
+      if(btn && btn.length){
+        changePage(btn.find('a')[0]);
+      }
+    }
+  });
+  $('.btn-grid a').on('click', function(ev){
+    if(ev.target.classList.hasClass('prev') || ev.target.classList.hasClass('prev')){
+      /* follow the link for prev/next page links */
+      return(true);
+    }
+    changePage(ev.target);
+    ev.preventDefault();
+    return(false);
+  });
+  /*$('.btn p').on('mouseenter', function(ev){
+
+  }); */
+  $('.tooltip').tooltipster();
 });

routes.py
@@ -10,7 +10,7 @@ class Route(object):
 routes = {
     "dash-mpd":Route(r'/dash/<manifest:[\w\-_]+\.mpd>', handler='views.LiveManifest', title='DASH test stream'),
     "dash-media":Route(r'/dash/<mode:(live|vod)>/<filename:(A[1-2]|V[1-3]|V3ENC)>/<segment:(\d+|init)>.<ext:(mp4|m4v|m4a|m4s)>', handler='views.LiveMedia', title="DASH fragment"),
-    "video":Route(r'/video', handler='views.VideoPlayer', title='DASH test stream player'),
+    "video":Route(r'/video/<testcase:[\w\-_]+>', handler='views.VideoPlayer', title='DASH test stream player'),
     "home":Route(r'/', handler='views.MainPage', title='DASH test streams'),
     "time":Route(r'/time/<format:(xsd|iso|ntp)>', handler='views.UTCTimeHandler', title='Current time of day'),
     "uploadBlob":Route(r'/blob', handler='views.UploadHandler', title='Upload blob'),

static/css/video.css
@@ -1,102 +1,1 @@
-body,
-html {
-  background: black;
-}
-#vid-window {
-  width: 640px;
-  height: 266px;
-  margin: 1em;
-}
-#vid-window > video {
-  width: 100%;
-  height: 100%;
-}
-#vid-window .icon {
-  position: absolute;
-  z-index: 2;
-  right: 4em;
-  top: 2em;
-  color: #eee;
-  display: none;
-}
-#vid-window.paused .fa-pause {
-  display: block;
-}
-.status,
-.textbox,
-.source,
-#play-error {
-  position: absolute;
-  height: 1em;
-  font-size: 2em;
-  line-height: 1em;
-  padding: 0.2em;
-  color: white;
-  text-align: center;
-  background-color: rgba(63, 63, 63, 0.4);
-}
-.status {
-  top: 2em;
-  width: 6em;
-}
-.textbox,
-#play-error {
-  left: 0;
-  width: 100%;
-}
-.textbox > span {
-  margin-left: 1em;
-}
-.metadata {
-  bottom: 8em;
-}
-.canplay {
-  bottom: 6em;
-}
-.play {
-  bottom: 4em;
-}
-.resize {
-  bottom: 2em;
-}
-#status {
-  left: 2em;
-}
-#status .target,
-#status .actual {
-  margin-right: 1em;
-}
-#vstatus {
-  right: 2em;
-}
-.prefetch {
-  display: none;
-}
-.finished {
-  font-size: 5rem;
-  position: absolute;
-  top: 50%;
-  height: 1em;
-  margin-top: -0.5em;
-  width: 12em;
-  left: 50%;
-  margin-left: -6em;
-  color: #ff6666;
-  text-align: center;
-  font-family: cursive;
-}
-#play-error {
-  font-size: 3rem;
-  color: red;
-  top: 6rem;
-}
-@media only screen and (min-width: 1280px) {
-  #vid-window {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 1280px;
-    height: 720px;
-    background: black;
-  }
-}
+body,html{background:#000;width:100%;height:100%}header{z-index:2}#vid-window{width:640px;height:266px;margin:1em;z-index:0}#vid-window>video{width:100%;height:100%}#vid-window .icon{position:absolute;z-index:2;right:4em;top:2em;color:#eee;display:none}#vid-window.paused .fa-pause{display:block}#play-error,#status,.source,.textbox{position:absolute;height:1em;left:2em;font-size:2em;line-height:1em;padding:0 .2em;color:#fff;text-align:center;background-color:rgba(63,63,63,.4);z-index:2}#status,.source{text-align:left}.source{top:3em;width:90%;height:1em;line-height:1em;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}#status{font-size:1.5rem;bottom:4em;width:25em;height:5em;overflow-y:hidden}#status .actual,#status .target{margin-right:1em}#play-error,.textbox{left:0;width:100%}.textbox>span{margin-left:1em}.metadata{bottom:8em}.canplay{bottom:6em}.play{bottom:4em}.resize{bottom:2em}#vstatus{right:2em}.prefetch{display:none}.finished{font-size:5rem;position:absolute;top:50%;height:1em;margin-top:-.5em;width:12em;left:50%;margin-left:-6em;color:#f66;text-align:center;font-family:cursive}#play-error{font-size:3rem;color:red;top:6rem}@media only screen and (min-height:576px){#vid-window{position:absolute;top:0;left:0;width:1024px;height:576px;background:#000}}@media only screen and (min-width:1280px){#vid-window{position:absolute;top:0;left:0;width:1280px;height:720px;background:#000}}

static/css/video.less
@@ -1,11 +1,18 @@
 body, html {
   background: black;
+  width: 100%;
+  height: 100%;
+}
+
+header {
+    z-index: 2;
 }
 
 #vid-window {
-		width: 640px;
-		height: 266px;
-		margin: 1em;
+    width: 640px;
+    height: 266px;
+    margin: 1em;
+    z-index: 0;
 }
 
 #vid-window > video {
@@ -14,31 +21,52 @@ body, html {
 }
 
 #vid-window .icon {
-		position: absolute;
-		z-index: 2;
-		right: 4em;
-		top: 2em;
-		color: #eee;
-		display: none;
+    position: absolute;
+    z-index: 2;
+    right: 4em;
+    top: 2em;
+    color: #eee;
+    display: none;
 }
 #vid-window.paused .fa-pause {
-		display: block;
+    display: block;
 }
 
-.status, .textbox, .source {
+#status, .textbox, .source {
     position: absolute;
     height: 1em;
+    left: 2em;
     font-size: 2em;
     line-height: 1em;
-    padding: 0.2em;
+    padding: 0 0.2em;
     color: white;
     text-align: center;
-    background-color: rgba(63,63,63,0.4);  
+    background-color: rgba(63,63,63,0.4);
+    z-index: 2;
 }
 
-.status {
-    top: 2em;
-    width: 6em;
+.source {
+    top: 3em;
+    width: 90%;
+    overflow: hidden;
+    height: 1em;
+    line-height: 1em;
+    text-align: left;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+#status {
+    font-size: 1.5rem;
+    bottom: 4em;
+    width: 25em;
+    height: 5em;
+    text-align: left;
+    overflow-y: hidden;
+    .target, .actual {
+      margin-right: 1em;
+    }
 }
 
 .textbox {
@@ -47,7 +75,7 @@ body, html {
   > span {
     margin-left: 1em;
   }
-} 
+}
 
 .metadata {
   bottom: 8em;
@@ -62,20 +90,12 @@ body, html {
   bottom: 2em;
 }
 
-
-#status {
-    left: 2em;
-		.target, .actual {
-		  margin-right: 1em;
-		}
-}
-
 #vstatus {
   right: 2em;
-}	
+}
 
 .prefetch {
-		display: none;
+    display: none;
 }
 
 .finished {
@@ -101,13 +121,24 @@ body, html {
   top: 6rem;
 }
 
+@media only screen and (min-height: 576px) {
+  #vid-window {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 1024px;
+      height: 576px;
+      background: black;
+    }
+}
+
 @media only screen and (min-width: 1280px) {
   #vid-window {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 1280px;
-			height: 720px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 1280px;
+      height: 720px;
       background: black;
-		}
+    }
 }

templates/video.html
@@ -11,80 +11,106 @@
 {% block content %}
 {% if source %}
 <p class="source"><a href="{{source}}">{{source}}</a></p>
+<div id="status" ></div>
 <div id="vid-window">
-	<span class="fa fa-4x fa-pause icon"></span>
+  <span class="fa fa-4x fa-pause icon"></span>
 </div>
 {% else %}
 <div id="vid"><span class="fa fa-4x fa-pause icon"></span></div>
-<div id="status" class="status"><span class="timecode">00:00:00</span></div>
+<div id="status" ><span class="timecode">00:00:00</span></div>
 {% endif %}
 {% endblock %}
 
 {% block scripts %}
-	{{import_script("jquery-1.11.1")}}
-    <script src="/js/dash.all.js" type="text/javascript"></script>
+  {{import_script("jquery-1.11.1")}}
+    <script src="/js/prod/shaka-player.js" type="text/javascript"></script>
 <script type="text/javascript">
-	var windowKeys = Object.keys(window);
+  var windowKeys = Object.keys(window);
 </script>
 <script type="text/javascript">
 $('body').on('keydown', function(ev){
-	'use strict';
-	switch(ev.which){
-		case 48: /* 0 key */
-	 	$('video').each(function(){
-	 		this.pause();
-	 		this.src='';
-	 		delete this.src;
-	 		this.load();
-	 		$(this).remove();
-	 	});
-		break;
-	case 49: /* 1 key */
-		window.open('','_self');
-		window.close();
-		break;
-	case 415: /* 'play' RCU key */
-		$('#vid').removeClass('paused');
-		vid.play();
-		break;
-	case 19: /* 'pause' RCU key */
-		$('#vid').addClass('paused');
-		vid.pause();
-		break;
-	}
+  'use strict';
+  switch(ev.which){
+    case 48: /* 0 key */
+     $('video').each(function(){
+       this.pause();
+       this.src='';
+       delete this.src;
+       this.load();
+       $(this).remove();
+     });
+    break;
+  case 49: /* 1 key */
+    window.open('','_self');
+    window.close();
+    break;
+  case 415: /* 'play' RCU key */
+    $('#vid').removeClass('paused');
+    vid.play();
+    break;
+  case 19: /* 'pause' RCU key */
+    $('#vid').addClass('paused');
+    vid.pause();
+    break;
+  }
 });
 $(window).load(function(){
-	var player, src, vid, context;
-	var k, wk = Object.keys(window);
-	for(k in wk){
-		if(! (k in windowKeys)){
-			console.log('new key '+k);
-		}
-	}
-	vid = document.createElement('video');
-	if(vid.canPlayType('{{mimeType}}')){
-		src = document.createElement('source');
-		src.setAttribute('type','{{mimeType}}');
-		src.setAttribute('src','{{source}}');
-		$(src).on('error', function(ev){
-			console.error('playback failed');
-			console.dir(ev);
-		});
-		$(vid).on('canplay',function(){ this.play(); });
-		vid.appendChild(src);
-		$('#vid-window').append(vid);
-		vid.load();
-	}
-	else{
-		/* fall-back to use DASH-IF JavaScript player */
-		$('#vid-window').append(vid);
-        context = new Dash.di.DashContext();
-        player = new MediaPlayer(context);
-        player.startup();
-        player.attachView(vid);
-        player.setAutoPlay(false);
-        player.attachSource('{{source}}');
-	}
+  'use strict'
+  var player, src, vid, context, estimator, evname;
+  var k, wk = Object.keys(window);
+  var statusEvents=['stalled','loadedmetadata', 'error', 'canplay', 'canplaythrough',
+  'playing', 'ended', 'pause', 'resize', 'loadstart'];
+
+  function genStatusFn(evName){
+    return function(ev){
+        var msg = new Date().toISOString()+': '+evName;
+        $('#status').append('<p>'+msg+'</p>');
+        console.log(msg);
+        while($('#status > p').length>5){
+            $('#status > p').first().remove();
+        }
+    };
+  }
+
+  for(k in wk){
+    if(! (k in windowKeys)){
+      console.log('new key '+k);
+    }
+  }
+  vid = document.createElement('video');
+  if(vid.canPlayType('{{mimeType}}')){
+    src = document.createElement('source');
+    src.setAttribute('type','{{mimeType}}');
+    src.setAttribute('src','{{source}}');
+    $(src).on('error', function(ev){
+      console.error('playback failed');
+      console.dir(ev);
+    });
+    $(vid).on('canplay',function(){ this.play(); });
+    vid.appendChild(src);
+    $('#vid-window').append(vid);
+    vid.load();
+  }
+  else{
+    /* fall-back to use Shaka JavaScript player */
+    $('#vid-window').append(vid);
+    shaka.polyfill.installAll();
+    player = new shaka.player.Player(vid);
+    player.addEventListener('error', function(ev){
+      console.error('playback failed');
+      console.dir(ev);
+    });
+    estimator = new shaka.util.EWMABandwidthEstimator();
+    $(vid).on('canplay',function(){ this.play(); });
+/*    player.addEventListener('canplay',function(){
+        this.play();
+    });*/
+    player.load(new shaka.player.DashVideoSource('{{source}}', null, estimator));
+  }
+  for(k=0; k<statusEvents.length; ++k){
+      evname = statusEvents[k];
+      $(vid).on(evname, genStatusFn(evname));
+  }
 });
 </script>
 {% endblock %}

views.py
@@ -5,7 +5,6 @@ try:
     import cStringIO as StringIO
 except ImportError:
     import StringIO
-
 import webapp2, jinja2
 from google.appengine.api import users, memcache
 from google.appengine.ext import blobstore
@@ -656,15 +655,42 @@ class LiveMedia(RequestHandler): #blobstore_handlers.BlobstoreDownloadHandler):
 
 class VideoPlayer(RequestHandler):
     """Responds with an HTML page that contains a video element to play the specified MPD"""
-    def get(self, **kwargs):
+    def get(self, testcase, **kwargs):
         try:
-            mpd_url = self.request.params['url']
+            testcase = testcases.testcase_map[testcase]
+            manifest = testcases.manifests[testcase['manifest']]
         except KeyError:
-            self.response.write('URL not specified')
+            self.response.write('Unknown test case')
             self.response.set_status(404)
             return
         context = self.create_context(**kwargs)
-        context.update(self.calculate_dash_params(mpd_url=mpd_url))
+        mpd_url = self.uri_for('dash-mpd', manifest=testcase['manifest'])
+        try:
+            mode = testcase['mode']
+        except KeyError:
+            try:
+                mode = manifest['mode']
+            except KeyError:
+                mode = None
+        context.update(self.calculate_dash_params(mpd_url=mpd_url, mode=mode))
+        params=[]
+        try:
+            for k,v in testcase['params'].iteritems():
+                if isinstance(v,(int,long)):
+                    params.append('%s=%d'%(k,v))
+                else:
+                    params.append('%s=%s'%(k,urllib.quote_plus(v)))
+        except KeyError:
+            pass
+        if testcase.get('corruption',False)==True:
+            err_time = context['now'].replace(microsecond=0) + datetime.timedelta(seconds=20)
+            times=[]
+            for i in range(10):
+                err_time += datetime.timedelta(seconds=10)
+                times.append(err_time.time().isoformat()+'Z')
+            params.append('vcorrupt=%s'%(urllib.quote_plus(','.join(times))))
+        if params:
+            mpd_url += '?' + '&'.join(params)
         context['source'] = urlparse.urljoin(self.request.host_url,mpd_url)
         if context['encrypted']:
             try:
@@ -672,8 +698,10 @@ class VideoPlayer(RequestHandler):
             except AttributeError:
                 pass
         context['mimeType'] = 'application/dash+xml'
-        if self.request.params.has_key('title'):
-            context['title'] = self.request.params['title']
+        try:
+            context['title'] = testcase['title']
+        except KeyError:
+            context['title'] = manifest['title']
         template = templates.get_template('video.html')
         self.response.write(template.render(context))
 

commit 41d17cc20b377e175235e871a9e967d50785cebc

fix clicking on link
Clicking on a link on the index page was causing the browser to
redirect to /video/undefined. This is a regression caused by commit
2a324ef6afd83c39d462029d36a597f4e23ca0a3.

This was due to two different bugs:
1. The logic to detect the previous and next pages was incorrect, as
   the class name was applied to the containing td element, not the
   anchor tag.
2. When clicking on a test case, the target of the event was the
   tooltip, rather than the anchor tag.

static/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
   'use strict';
   function changePage(anchor){
-    var $a = $(anchor);
-    document.location.assign('/video/'+$a.data('testcase'));
+    var tc, $a = $(anchor);
+    tc = $a.data('testcase');
+    if(tc!==undefined){
+        document.location.assign('/video/'+tc);
+    }
   }
 
   $('body').on('keydown', function(ev){
@@ -30,11 +33,15 @@ $(document).ready(function(){
     }
   });
   $('.btn-grid a').on('click', function(ev){
-    if(ev.target.classList.contains('prev') || ev.target.classList.contains('prev')){
+    var anchor = ev.target;
+    while(anchor.tagName!='A' && anchor.tagName!='TR'){
+        anchor = anchor.parentNode;
+    }
+    if(anchor.classList.contains('prev') || anchor.classList.contains('next')){
       /* follow the link for prev/next page links */
       return(true);
     }
-    changePage(ev.target);
+    changePage(anchor);
     ev.preventDefault();
     return(false);
   });

templates/index.html
@@ -9,7 +9,7 @@
   <table class="btn-grid table">
     <tbody>
   {% if page>1 %}
-      <tr class="prev-next"><td colspan="4" id="btn0" class="prev"><a href="{{uri_for("home")}}?page={{page-1}}"><span class="key">0:</span>Previous page</a></td></tr>
+      <tr class="prev-next"><td colspan="4" id="btn0" class="prev"><a href="{{uri_for("home")}}?page={{page-1}}" class="prev"><span class="key">0:</span>Previous page</a></td></tr>
   {% endif %}
       {% for row in rows %}
         <tr><th class="title"><p class="title">{{row.title}}</p>{% for sub in row.details %}<p class="subtitle">{{sub|escape}}</p>{% endfor %}</th>
@@ -27,7 +27,7 @@
         </tr>
       {% endfor %}
   {% if page<num_pages %}
-      <tr class="prev-next"><td colspan="4" id="btn9" class="next"><a href="{{uri_for("home")}}?page={{page+1}}"><span class="key">9:</span>Next page</a></td></tr>
+      <tr class="prev-next"><td colspan="4" id="btn9" class="next"><a class="next" href="{{uri_for("home")}}?page={{page+1}}"><span class="key">9:</span>Next page</a></td></tr>
   {% endif %}
     </tbody>
   </table>