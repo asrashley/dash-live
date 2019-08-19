264a50ee3e747bac3391d2e23890019a183329ee
Alex Ashley <alex.ashley@youview.com>
2019-08-19 15:02:34 +0100

    remove host checking when running in dev mode

--- runserver.bat
+++ runserver.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist .gaedata mkdir .gaedata
 rem python "c:\Program Files (x86)\Google\google_appengine\dev_appserver.py" --port=9080 --host=0.0.0.0 --datastore_path=.gaedata\datastore.sql3 --clear_datastore true app.yaml 
-dev_appserver.py --port=9080 --host=0.0.0.0 --datastore_path=.gaedata\datastore.sql3 app.yaml
+dev_appserver.py --port=9080 --host=0.0.0.0 --datastore_path=.gaedata\datastore.sql3 --enable_host_checking False app.yaml
 rem python "..\google_appengine\dev_appserver.py" --port=9080 --host=0.0.0.0 --datastore_path=.gaedata\datastore.sql3 app.yaml 
--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,4 @@ if [ ! -d .gaedata ]; then
   mkdir .gaedata
 fi
 #dev_appserver.py --port=9080 --host=0.0.0.0 --storage_path=.gaedata --dev_appserver_log_level=debug app.yaml 
-exec dev_appserver.py --port=9080 --host=0.0.0.0 --storage_path=.gaedata --dev_appserver_log_level=info app.yaml 
+exec dev_appserver.py --port=9080 --host=0.0.0.0  --admin_host=0.0.0.0 --admin_port=9081 --storage_path=.gaedata --dev_appserver_log_level=info app.yaml 
