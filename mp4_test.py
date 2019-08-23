7b899fe60aa2dfb4f03a0b7fa6b619459be87e56
Alex Ashley <alex.ashley@youview.com>
2019-08-23 10:03:40 +0100

    added apache 2 license to project

--- mp4_test.py
+++ mp4_test.py
@@ -1,3 +1,25 @@
+#############################################################################
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#############################################################################
+#
+#  Project Name        :    Simulated MPEG DASH service
+#
+#  Author              :    Alex Ashley
+#
+#############################################################################
+
 import base64
 import binascii
 import os
@@ -132,8 +154,6 @@ class DrmTest(unittest.TestCase):
         moof.mfhd.sequence_number = segment_num
         data = moof.encode()
         new_moof = mp4.Mp4Atom.create(StringIO.StringIO(data))[0]
-        new_moof.dump()
-        print(moof.traf.tfdt)
         self.assertBuffersEqual(expected_data, data)
         
     def test_wrap_boxes(self):
