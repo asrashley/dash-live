8ae0f60693ebf71e02216fdff14ba4b42cfa537b
asrashley <alex@ashley-family.net>
2019-09-06 13:45:39 +0100

    turn off host checking in development mode

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 if [ -z "${GAEDATA}" ]; then
     export GAEDATA="${HOME}/.gaedata"
 fi
 if [ ! -d ${GAEDATA} ]; then
   mkdir ${GAEDATA}
 fi
-exec dev_appserver.py --port=9080 --host=0.0.0.0  --admin_host=0.0.0.0 --admin_port=9081 --storage_path=${GAEDATA} --dev_appserver_log_level=info app.yaml 
+if [ -z "${PORT}" ]; then
+    PORT=9080
+fi
+let ADMIN_PORT=$(( $PORT + 1))
+HOST_CHECKING=""
+if dev_appserver.py -h | grep -q enable_host_checking ; then
+  HOST_CHECKING="--enable_host_checking False"
+fi
+exec dev_appserver.py --port=${PORT} --host=0.0.0.0  --admin_host=0.0.0.0 --admin_port=${ADMIN_PORT} --storage_path=${GAEDATA} --dev_appserver_log_level=info ${HOST_CHECKING} app.yaml 
