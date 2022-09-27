9c9577129307e6f4b29eb924fe61d3b39cc6a212
Alex Ashley <alex@ashley-family.net>
2022-09-27 14:01:44 +0100

    split bitio.py into submodule with each class in its own file

---
+++ src/utils/bitio/bits_field_reader.py
@@ -0,0 +1,101 @@
+#############################################################################
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#############################################################################
+#
+#  Project Name        :    Simulated MPEG DASH service
+#
+#  Author              :    Alex Ashley
+#
+#############################################################################
+
+import logging
+import os
+import sys
+
+try:
+    import bitstring
+except ImportError:
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "..", "lib")))
+    import bitstring
+
+class BitsFieldReader(object):
+    def __init__(self, name, src, kwargs, size=None, data=None, debug=False):
+        self.name = name
+        self.debug = debug
+        bitsize = None
+        if isinstance(src, BitsFieldReader):
+            if size is None:
+                bitsize = src.bitsize - src.bitpos()
+                size = bitsize // 8
+            if data is None:
+                data = src.data
+            src = src.src
+        if size is None:
+            if data is None:
+                try:
+                    size = kwargs["size"] - kwargs["header_size"]
+                except KeyError:
+                    pos = src.tell()
+                    src.seek(0, 2)  # seek to end
+                    size = src.tell() - pos
+                    src.seek(pos)
+            else:
+                size = len(data)
+        if data is None:
+            self.data = src.read(size)
+            self.src = bitstring.ConstBitStream(bytes=self.data)
+        else:
+            self.data = data
+            self.src = src
+        self.kwargs = kwargs
+        if bitsize is None:
+            bitsize = 8 * size
+        self.bitsize = bitsize
+        if debug:
+            self.log = logging.getLogger('bitio')
+        else:
+            self.log = None
+
+    def duplicate(self, name, kwargs):
+        return BitsFieldReader(name, self.src, kwargs, debug=self.debug,
+                               size=len(self.data), data=self.data)
+
+    def read(self, size, field):
+        self.kwargs[field] = self.get(size, field)
+
+    def read_bytes(self, length, field):
+        if self.log:
+            self.log.debug(
+                '%s: read_bytes %s size=%d pos=%s', self.name, field, length,
+                self.src.pos)
+        data = self.src.read('bytes:{0}'.format(length))
+        self.kwargs[field] = data
+
+    def get(self, size, field):
+        if self.log:
+            self.log.debug(
+                '%s: read %s size=%d pos=%s', self.name, field, size,
+                self.src.bitpos)
+        if size == 1:
+            return self.src.read('bool')
+        return self.src.read('uint:%d' % size)
+
+    def bitpos(self):
+        return self.src.bitpos
+
+    def bytepos(self):
+        return self.src.bytepos
---
+++ src/utils/bitio/bits_field_writer.py
@@ -0,0 +1,98 @@
+#############################################################################
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#############################################################################
+#
+#  Project Name        :    Simulated MPEG DASH service
+#
+#  Author              :    Alex Ashley
+#
+#############################################################################
+
+import logging
+import os
+import sys
+
+try:
+    import bitstring
+except ImportError:
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "..", "lib")))
+    import bitstring
+
+
+class BitsFieldWriter(object):
+    def __init__(self, obj, dest=None):
+        self.obj = obj
+        if dest is None:
+            self.bits = bitstring.BitArray()
+        elif isinstance(dest, BitsFieldWriter):
+            self.bits = dest.bits
+        else:
+            self.bits = dest
+        if getattr(obj, 'debug', False):
+            self.log = logging.getLogger('bitio')
+        else:
+            self.log = None
+
+    def duplicate(self, new_obj):
+        return BitsFieldWriter(obj=new_obj, dest=self.bits)
+
+    def write(self, size, field, value=None):
+        if value is None:
+            value = getattr(self.obj, field)
+        if self.log:
+            self.log.debug(
+                '%s: write %s size=%d pos=%d value=0x%x',
+                self.obj.classname, field, size, self.bits.len, value)
+        self.bits.append(bitstring.Bits(uint=value, length=size))
+
+    def write_bytes(self, field, length=None, value=None):
+        if value is None:
+            value = getattr(self.obj, field)
+        if length is None:
+            length = len(value)
+        if self.log:
+            self.log.debug(
+                '%s: write_bytes %s size=%d pos=%d',
+                self.obj.classname, field, length, self.bits.len)
+        self.bits.append(bitstring.Bits(bytes=value, length=(8 * length)))
+
+    def append_writer(self, field_writer):
+        self.bits.append(field_writer.bits)
+
+    def overwrite(self, position, size, field, value=None):
+        if value is None:
+            value = getattr(self.obj, field)
+        if self.log:
+            self.log.debug(
+                '%s: overwrite %s size=%d pos=%d value=0x%x',
+                self.obj.classname, field, size, position, value)
+        data = bitstring.Bits(uint=value, length=size)
+        self.bits.overwrite(data, position)
+
+    def bitpos(self):
+        return self.bits.len
+
+    def bytepos(self):
+        bitpos = self.bitpos()
+        assert((bitpos & 8) == 0)
+        return bitpos // 8
+
+    def toBytes(self):
+        return self.bits.bytes
+
+    def __len__(self):
+        return self.bits.len
