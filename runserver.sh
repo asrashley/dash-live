a69228e005ffeec61854e041ae007a3645cb53b0
Alex Ashley <alex@ashley-family.net>
2023-05-11 16:12:08 +0100

    allow appserver log level to be configured
    When using runserver.sh to start the app, it might be useful to be
    able to modify the logging level. This commit adds a LOG_LEVEL
    to the runserver.sh script and the Docker container that uses this
    script.

--- runserver.sh
+++ runserver.sh
@@ -8,10 +8,13 @@ fi
 if [ -z "${PORT}" ]; then
     PORT=9080
 fi
+if [ -z "${LOG_LEVEL}" ]; then
+    LOG_LEVEL="info"
+fi
 let ADMIN_PORT=$(( $PORT + 1))
 HOST_CHECKING=""
 if dev_appserver.py -h | grep -q enable_host_checking ; then
   HOST_CHECKING="--enable_host_checking False"
 fi
 python -m lesscpy static/css -o static/css/
-exec dev_appserver.py --port=${PORT} --host=0.0.0.0  --admin_host=0.0.0.0 --admin_port=${ADMIN_PORT} --storage_path=${GAEDATA} --dev_appserver_log_level=info ${HOST_CHECKING} app.yaml 
+exec dev_appserver.py --port=${PORT} --host=0.0.0.0  --admin_host=0.0.0.0 --admin_port=${ADMIN_PORT} --storage_path=${GAEDATA} --dev_appserver_log_level=${LOG_LEVEL} ${HOST_CHECKING} app.yaml
