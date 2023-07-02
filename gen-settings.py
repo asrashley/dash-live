f912c1ba03ed58a70a74878b3972043dcc133475
Alex Ashley <github@ashley-family.net>
2023-07-02 13:23:42 +0100

    remove usage of GoogleAppEngine
    gen-settings was creating a settings file that was still based upon
    usage of GAE.

--- gen-settings.py
+++ gen-settings.py
@@ -4,15 +4,14 @@ import random
 import string
 import os
 
-TEMPLATE="""from server.gae import on_production_server
-
+TEMPLATE="""from os import environ
 cookie_secret = r'{cookie}'
 csrf_secret = r'{csrf}'
 jwt_secret = r'{jwt}'
 default_admin_username = 'admin'
 default_admin_password = r'{password}'
-DEBUG = not on_production_server
 allowed_domains = "*"
+DEBUG = environ.get("LOG_LEVEL", "error").lower() == 'debug'
 """
 
 def make_random_string(length: int) -> str:
@@ -28,6 +27,7 @@ jwt = make_random_string(20)
 password = make_random_string(10)
 
 if not os.path.exists("dashlive/server/settings.py"):
+    print(f'Creating settings.py with default admin account username="admin" password ="{password}"')
     with open('dashlive/server/settings.py', 'wt', encoding='ascii') as out:
         out.write(TEMPLATE.format(**locals()))
 
