594bea8412b795201a48df621f25c62c6e43ab1d
Alex Ashley <github@ashley-family.net>
2023-07-02 14:53:07 +0100

    update to use Flask rather than GAE

--- runserver.bat
+++ runserver.bat
@@ -1,5 +1,7 @@
 @echo off
-SETLOCAL ENABLEEXTENSIONS
-SET GAEDATA=%LOCALAPPDATA%\.gaedata
-if not exist %GAEDATA% mkdir %GAEDATA%
-dev_appserver.py --port=9080 --host=0.0.0.0 --admin_host=0.0.0.0 --admin_port=9081 --datastore_path=%GAEDATA%\datastore.sql3 --enable_host_checking False app.yaml
+python -m lesscpy static/css -o static/css/
+set LC_ALL=C.UTF-8
+set LANG=C.UTF-8
+set FLASK_APP="dashlive.server.app"
+
+python -m flask --app dashlive.server.app run --host=0.0.0.0
