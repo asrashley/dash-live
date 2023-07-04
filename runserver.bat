4672cdc4773fd0dbfec522b357b768559b4310a2
Alex Ashley <github@ashley-family.net>
2023-07-04 17:32:24 +0100

    add debug flag so that Flask watches for file changes

--- runserver.bat
+++ runserver.bat
@@ -4,4 +4,4 @@ set LC_ALL=C.UTF-8
 set LANG=C.UTF-8
 set FLASK_APP="dashlive.server.app"
 
-python -m flask --app dashlive.server.app run --host=0.0.0.0
+python -m flask --app dashlive.server.app run --host=0.0.0.0 --debug
