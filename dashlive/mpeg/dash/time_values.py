9097cb0ee51682ecf921199b8f5c613dba7bf209
Alex Ashley <github@ashley-family.net>
2023-08-23 22:35:25 +0100

    remove unused TimeValues class

--- dashlive/mpeg/dash/time_values.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime, timedelta
-
-@dataclass(slots=True, frozen=True)
-class TimeValues:
-    mode: str
-    elapsedTime: datetime
-    timeShiftBufferDepth: timedelta
-    mediaDuration: timedelta
