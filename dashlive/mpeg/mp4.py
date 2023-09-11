102b2328bb10ad963bd8a8c3109c9c6e632da89f
Alex Ashley <github@ashley-family.net>
2023-09-11 18:48:20 +0100

    add UserDataBox

--- dashlive/mpeg/mp4.py
+++ dashlive/mpeg/mp4.py
@@ -1056,6 +1056,10 @@ class SampleTableBox(BoxWithChildren):
     pass
 
 
+@fourcc('udta')
+class UserDataBox(BoxWithChildren):
+    pass
+
 @fourcc('mvhd')
 class MovieHeaderBox(FullBox):
     OBJECT_FIELDS = {
