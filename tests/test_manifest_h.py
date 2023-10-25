8053837ff42d7678ecf147342d6a7092ab6358aa
Alex Ashley <github@ashley-family.net>
2023-10-25 22:21:31 +0100

    migrate manifest_h tests to async

--- tests/test_manifest_h.py
+++ tests/test_manifest_h.py
@@ -19,17 +19,19 @@
 #  Author              :    Alex Ashley
 #
 #############################################################################
+import datetime
 import unittest
 
 from .mixins.check_manifest import DashManifestCheckMixin
 from .mixins.flask_base import FlaskTestBase
 
 class ManifestHTest(FlaskTestBase, DashManifestCheckMixin):
-    def test_manifest_h_vod(self):
-        self.check_a_manifest_using_major_options('manifest_h.mpd', 'vod')
+    async def test_manifest_h_vod(self):
+        await self.check_a_manifest_using_major_options('manifest_h.mpd', 'vod')
 
-    def test_manifest_h_live(self):
-        self.check_a_manifest_using_major_options('manifest_h.mpd', 'live')
+    @FlaskTestBase.mock_datetime_now(datetime.datetime.fromisoformat("2023-09-10T17:56:43Z"))
+    async def test_manifest_h_live(self):
+        await self.check_a_manifest_using_major_options('manifest_h.mpd', 'live')
 
 
 if __name__ == '__main__':
