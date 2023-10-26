9cd63afa85fd630ad5d162f3a8fc6c40373db02a
Alex Ashley <github@ashley-family.net>
2023-10-26 21:07:02 +0100

    export ValidationFlag

--- dashlive/mpeg/dash/validator/__init__.py
+++ dashlive/mpeg/dash/validator/__init__.py
@@ -3,11 +3,13 @@ from .http_client import HttpClient
 from .options import ValidatorOptions
 from .pool import WorkerPool
 from .validator import DashValidator
+from .validation_flag import ValidationFlag
 
 __all__ = [
     ConcurrentWorkerPool,
     DashValidator,
     HttpClient,
+    ValidationFlag,
     ValidatorOptions,
     WorkerPool
 ]
