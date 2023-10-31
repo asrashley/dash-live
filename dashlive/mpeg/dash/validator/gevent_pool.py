e1be621bd3fb87261da532e0a1a522f3e11e48e0
Alex Ashley <github@ashley-family.net>
2023-10-31 20:56:03 +0000

    use threads rather than greenlets for socket.io and validator
    Trying to get both socket.io WebSocket server and the DASH validator
    working using gevent and asyncio was not producing stable results.
    Using threads with asyncio appears to be much more stable and the
    performance seems acceptable.

--- dashlive/mpeg/dash/validator/gevent_pool.py
+++ dashlive/mpeg/dash/validator/gevent_pool.py
@@ -3,21 +3,56 @@ import asyncio
 import asyncio_gevent
 import gevent
 import gevent.pool
+from gevent.threadpool import ThreadPoolExecutor
 
 from .pool import WorkerPool
 
+class AsyncPoolContextManager:
+    next_id = 1
+
+    def __init__(self, executor: ThreadPoolExecutor) -> None:
+        self.executor = executor
+        self.tasks = []
+        self.tid = self.__class__.next_id
+        self.__class__.next_id += 1
+
+    async def __aenter__(self):
+        print(f'enter TaskGroup id={self.tid}')
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        print(f'TaskGroup await id={self.tid} len=', len(self.tasks))
+        print(await asyncio.gather(*self.tasks, return_exceptions=True))
+
+    def submit(self, async_fn, *args, **kwargs) -> asyncio.Future:
+        print(f'TaskGroup submit id={self.tid}')
+        try:
+            sync_fn = asyncio_gevent.async_to_sync(async_fn)
+            future = self.executor.submit(sync_fn, *args, **kwargs)
+            print(f'submit task id={self.tid} future=', type(future))
+            self.tasks.append(future)
+            print(f'TaskGroup submit id={self.tid} success', len(self.tasks))
+        except Exception as err:
+            print('Submit exception', type(err), err)
+            raise
+        return future
+
+
 class GeventWorkerPool(WorkerPool):
-    def __init__(self, maxsize: int) -> None:
-        if maxsize < 1:
-            self.pool = gevent.get_hub().threadpool
-        else:
-            self.pool = gevent.pool.Pool(maxsize, hub=gevent.get_hub())
-        self.tasks = set()
+    def __init__(self, executor: ThreadPoolExecutor) -> None:
+        self.executor = executor
+
+    def group(self) -> AsyncPoolContextManager:
+        """
+        Creates a new task group that will wait for their completion when
+        the context manager exits.
+        """
+        return AsyncPoolContextManager(self.executor)
 
     def submit(self, fn, *args, **kwargs) -> asyncio.Future:
-        greenlet = self.pool.apply_async(fn, *args, **kwargs)
-        self.tasks.add(greenlet)
-        future = asyncio_gevent.greenlet_to_future(greenlet)
+        print('submit task', fn)
+        future = self.executor.submit(fn, *args, **kwargs)
+        print('submit task future=', type(future))
         return future
 
     def wait_for_completion(self) -> list[str]:
