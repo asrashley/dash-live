36608bf8619901969434fe320c2d0298fed89d7d
Alex Ashley <alex@ashley-family.net>
2023-11-01 20:08:17 +0000

    switch from uwsgi to gunicorn when using nginx
    The move from greenlet to threads for socket.io has caused the
    DASH validator to fail when running in a Docker container. This
    commit switches to using gunicorn, which has an option to use
    threads.

--- deploy/create_virtenv.sh
+++ deploy/create_virtenv.sh
@@ -6,4 +6,5 @@ fi
 python3 -m venv /home/dash/.venv
 source /home/dash/.venv/bin/activate
 pip3 install --prefix /home/dash/.venv -r /home/dash/dash-live/requirements.txt
-pip3 install uwsgi==2.0.22
+#pip3 install uwsgi==2.0.22
+pip3 install gunicorn==21.2.0
