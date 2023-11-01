d884378b59344e8c6042c40e3462264def16da1b
Alex Ashley <github@ashley-family.net>
2023-11-01 08:09:48 +0000

    use separate sessions for each WebSocket validation session
    If more than one browser was connected the validator, each would
    receive the same output.
    
    This commit puts all of the state information for a websock connection
    into an instance of ClientConnection class. This instance is used to
    respond to the client that opened the connection and no other client.

--- dashlive/server/requesthandler/websocket.py
+++ dashlive/server/requesthandler/websocket.py
@@ -35,34 +35,25 @@ from dashlive.server.thread_pool import pool_executor
 
 from .ws_log_handler import WebsocketLogHandler
 
-class WebsocketHandler(Progress):
-    def __init__(self, sockio) -> None:
+class ClientConnection(Progress):
+    def __init__(self, sockio, session_id) -> None:
         super().__init__()
         self.sockio = sockio
+        self.session_id = session_id
         self.dash_log = logging.getLogger('DashValidator')
         self.dash_log.propagate = False
         self._aborted = False
         self.tasks: set[Thread] = set()
         self.tmpdir: Optional[tempfile.TemporaryDirectory] = None
-        self.queue_handler: Optional[QueueHandler] = None
-        self.listener: Optional[QueueListener] = None
-
-    def connect(self) -> None:
-        self._aborted = False
-        self.join_finished_tasks()
-        if self.listener:
-            self.listener.stop()
-        if self.queue_handler:
-            self.dash_log.removeHandler(self.queue_handler)
-        # self.pool = GeventWorkerPool(pool_executor)
         self.pool = ConcurrentWorkerPool(pool_executor)
         log_queue = queue.Queue(-1)
         self.queue_handler = QueueHandler(log_queue)
         self.dash_log.addHandler(self.queue_handler)
-        self.listener = QueueListener(log_queue, WebsocketLogHandler(self.sockio))
+        self.listener = QueueListener(
+            log_queue, WebsocketLogHandler(sockio, session_id))
         self.listener.start()
 
-    def disconnect(self) -> None:
+    def shutdown(self) -> None:
         self._aborted = True
         self.join_finished_tasks()
         if self.listener:
@@ -96,9 +87,11 @@ class WebsocketHandler(Progress):
         elif cmd == 'save':
             self.save_cmd(data)
 
+    def emit(self, cmd, data) -> None:
+        self.sockio.emit(cmd, data, to=self.session_id)
+
     def send_progress(self, pct: float, text: str) -> None:
-        self.sockio.emit(
-            'progress', {'pct': round(pct), 'text': text}, to=flask.request.sid)
+        self.emit('progress', {'pct': round(pct), 'text': text})
 
     def aborted(self) -> bool:
         return self._aborted
@@ -123,33 +116,32 @@ class WebsocketHandler(Progress):
                     errs['prefix'] = f'"{data["directory"]}" directory already exists'
             if data['title'] == '':
                 errs['title'] = 'Title is required'
-        self.sockio.emit('manifest-validation', errs, to=flask.request.sid)
+        self.emit('manifest-validation', errs)
         if errs:
             return
         upload_dir = flask.current_app.config['UPLOAD_FOLDER']
         if self.tmpdir is not None:
-            self.sockio.emit('log', {
+            self.emit('log', {
                 'level': 'error',
                 'text': 'Saving stream already in progress'
-            }, to=flask.request.sid)
+            })
             return
-
         asyncio_loop.run_coroutine(
             self.dash_validator_task, pool=self.pool, upload_dir=upload_dir, **data)
 
     def cancel_cmd(self, data) -> None:
-        self.sockio.emit('log', {
+        self.emit('log', {
             'level': 'info',
             'text': 'Cancelled validation'
-        }, to=flask.request.sid)
+        })
         self._aborted = True
 
     def save_cmd(self, data) -> None:
         self._aborted = False
-        self.sockio.emit('log', {
+        self.emit('log', {
             'level': 'info',
             'text': 'Creating stream'
-        }, to=flask.request.sid)
+        })
         del data['method']
         self.save_stream_task(**data)
 
@@ -157,10 +149,10 @@ class WebsocketHandler(Progress):
             self, method: str, manifest: str, upload_dir: str, pool: WorkerPool,
             **kwargs) -> None:
         if self.tmpdir is not None:
-            self.sockio.emit('log', {
+            self.emit('log', {
                 'level': 'error',
                 'text': 'Saving stream already in progress'
-            }, to=flask.request.sid)
+            })
             return
         start_time = time.time()
         opts = ValidatorOptions(log=self.dash_log, progress=self, pool=pool, **kwargs)
@@ -177,28 +169,28 @@ class WebsocketHandler(Progress):
                 self.dash_log.error('loading manifest failed')
                 return
             self.dash_log.debug('loading manifest complete')
-            self.sockio.emit('manifest', {
+            self.emit('manifest', {
                 'text': dv.get_manifest_lines()
-            }, to=flask.request.sid)
+            })
             await dv.run()
             if dv.has_errors():
                 errs = [e.to_dict() for e in dv.get_errors()]
                 self.dash_log.info('Found %d errors', len(errs))
-                self.sockio.emit('manifest-errors', errs)
+                self.emit('manifest-errors', errs)
             duration = round(time.time() - start_time)
             txt = f'DASH validation complete after {duration:#5.1f} seconds'
             self.dash_log.info(txt)
-            self.sockio.emit('progress', {
+            self.emit('progress', {
                 'pct': 100,
                 'text': txt,
                 'finished': True
-            }, to=flask.request.sid)
+            })
             if opts.save:
-                self.sockio.emit('script', {
+                self.emit('script', {
                     'filename': dv.get_json_script_filename(),
                     'prefix': opts.prefix,
                     'title': opts.title,
-                }, to=flask.request.sid)
+                })
         except Exception as err:
             self.dash_log.error('%s', err)
 
@@ -211,11 +203,11 @@ class WebsocketHandler(Progress):
         self.dash_log.debug('Installing %s', filename)
         pd.populate_database(filename)
         self.dash_log.info('Adding new stream complete')
-        self.sockio.emit('progress', {
+        self.emit('progress', {
             'pct': 100,
             'text': f'Added stream "{title}" ({prefix}) to this server',
             'finished': True
-        }, to=flask.request.sid)
+        })
 
     def join_finished_tasks(self) -> None:
         done: set[Thread] = set()
@@ -231,3 +223,29 @@ class WebsocketHandler(Progress):
             self.tasks.remove(tsk)
         for tsk in done:
             tsk.join()
+
+class WebsocketHandler:
+    def __init__(self, sockio) -> None:
+        self.sockio = sockio
+
+    def connect(self) -> None:
+        logging.debug('WebSocket connection %s', flask.request.sid)
+
+    def disconnect(self) -> None:
+        if 'websock_connection' in flask.g:
+            if flask.g.websock_connection.session_id == flask.request.sid:
+                con = flask.g.pop('websock_connection', None)
+                if con:
+                    con.shutdown()
+
+    def event_handler(self, data: JsonObject) -> None:
+        if 'method' not in data:
+            self.sockio.emit('log', {
+                "level": "error",
+                "test": 'Invalid request - no method',
+            }, to=flask.request.sid)
+            return
+        if 'websock_connection' not in flask.g:
+            flask.g.websock_connection = ClientConnection(
+                self.sockio, flask.request.sid)
+        flask.g.websock_connection.event_handler(data)
--- dashlive/server/requesthandler/ws_log_handler.py
+++ dashlive/server/requesthandler/ws_log_handler.py
@@ -8,13 +8,14 @@
 import logging
 
 class WebsocketLogHandler(logging.Handler):
-    def __init__(self, sock, level=logging.NOTSET) -> None:
+    def __init__(self, sock, session_id: str, level=logging.NOTSET) -> None:
         super().__init__(level)
         self.sock = sock
+        self.session_id = session_id
 
     def emit(self, record: logging.LogRecord) -> None:
         msg = record.getMessage()
         self.sock.emit('log', {
             'level': record.levelname.lower(),
             'text': msg
-        })
+        }, to=self.session_id)
