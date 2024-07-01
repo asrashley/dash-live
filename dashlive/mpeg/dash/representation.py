f276c10ac6870d87ee70a31e6ac1e37885136301
Alex Ashley <github@ashley-family.net>
2024-07-01 16:39:42 +0100

    parse sample rate and channel count for encrypted EAC3 streams
    This commit adds an encrypted EAC-3 test fixture and fixes the
    Representation class to correctly find the audio sampling rate
    and number of channels in an encrypted E-AC3 audio mp4 file.

--- dashlive/mpeg/dash/representation.py
+++ dashlive/mpeg/dash/representation.py
@@ -298,14 +298,16 @@ class Representation(ObjectWithFields):
                 self.numChannels = 8
         elif avc_type == 'ec-3':
             try:
-                self.add_field('sampleRate', avc.sampling_frequency)
+                self.add_field('sampleRate', 48000)
                 self.add_field('numChannels', 0)
                 for s in avc.dec3.substreams:
+                    self.sampleRate = s.sampling_frequency
                     self.numChannels += s.channel_count
                     if s.lfeon:
                         self.numChannels += 1
             except AttributeError:
-                pass
+                self.add_field('sampleRate', avc.sampling_frequency)
+                self.add_field('numChannels', 0)
         elif avc_type == 'ac-3':
             try:
                 self.add_field('sampleRate', avc.dac3.sampling_frequency)
