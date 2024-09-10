7525b6828eb531cddf1cc3ae94ad03020020f90e
Alex Ashley <github@ashley-family.net>
2024-09-10 15:34:29 +0100

    add live and vod known-good fixtures for manifest_ef

--- tests/test_manifest_ef.py
+++ tests/test_manifest_ef.py
@@ -33,6 +33,16 @@ class ManifestEFTest(FlaskTestBase, DashManifestCheckMixin):
     async def test_manifest_ef_live(self):
         await self.check_a_manifest_using_major_options('manifest_ef.mpd', 'live')
 
+    @FlaskTestBase.mock_datetime_now(datetime.datetime.fromisoformat("2023-10-01T02:03:58Z"))
+    def test_generated_manifest_against_fixture_vod(self):
+        self.check_generated_manifest_against_fixture(
+            'manifest_ef.mpd', mode='vod', acodec='mp4a', encrypted=False)
+
+    @FlaskTestBase.mock_datetime_now(datetime.datetime.fromisoformat("2023-10-01T02:03:58Z"))
+    def test_generated_manifest_against_fixture_live(self):
+        self.check_generated_manifest_against_fixture(
+            'manifest_ef.mpd', mode='live', acodec='mp4a', encrypted=False)
+
 
 if __name__ == '__main__':
     unittest.main()
