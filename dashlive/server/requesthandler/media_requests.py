e64c105bd7f3cb30ff4058562f07898975bdcb99
Alex Ashley <github@ashley-family.net>
2024-09-19 17:26:13 +0100

    refactor live profile media requests to split init requests from media
    To simplify the code, split out the code paths that handle init segment
    requests from the code that handles media segments.

--- dashlive/server/requesthandler/media_requests.py
+++ dashlive/server/requesthandler/media_requests.py
@@ -28,6 +28,7 @@ import flask
 
 from dashlive.mpeg import mp4
 from dashlive.mpeg.dash.adaptation_set import AdaptationSet
+from dashlive.mpeg.dash.representation import Representation
 from dashlive.mpeg.dash.timing import DashTiming
 from dashlive.server import models
 from dashlive.server.events.factory import EventFactory
@@ -52,12 +53,12 @@ class OnDemandMedia(RequestHandlerBase):
 
     decorators = [uses_stream, uses_media_file]
 
-    def get(self, stream, filename, ext):
+    def get(self, stream: str, filename: str, ext: str) -> flask.Response:
         try:
             start, end, status, headers = self.get_http_range(current_media_file.blob.size)
         except ValueError as ve:
             logging.warning('Invalid HTTP range: %s', ve)
-            return flask.make_response(f'Invalid HTTP range "{ve}"', 400)
+            return flask.make_response('Invalid HTTP range', 400)
         if start is None:
             logging.warning('HTTP range not specified')
             return flask.make_response('HTTP range must be specified', 400)
@@ -66,7 +67,7 @@ class OnDemandMedia(RequestHandlerBase):
         elif ext == 'm4v':
             headers['Content-Type'] = 'video/mp4'
         else:
-            headers['Content-Type'] = 'applicable/mp4'
+            headers['Content-Type'] = 'application/mp4'
         data = b''
         if status == 206:
             with current_media_file.open_file(start=start) as reader:
@@ -88,6 +89,8 @@ class LiveMedia(RequestHandlerBase):
             ext: str,
             segment_num: str | None = None,
             segment_time: int | None = None) -> flask.Response:
+        seg_num: int | None = None
+
         logging.debug(
             'LiveMedia.get: %s.%s stream=%s num=%s time=%s',
             filename, ext, stream, segment_num, segment_time)
@@ -96,7 +99,7 @@ class LiveMedia(RequestHandlerBase):
             options = self.calculate_options(mode, flask.request.args, current_stream)
         except ValueError as err:
             logging.error('Invalid CGI parameters: %s', err)
-            return flask.make_response(f'Invalid CGI parameters: {err}', 400)
+            return flask.make_response('Invalid CGI parameters', 400)
         if current_stream.timing_reference is None:
             logging.warning('stream.timing_reference has not been configured')
             return flask.make_response(
@@ -107,119 +110,164 @@ class LiveMedia(RequestHandlerBase):
                 'Request for an encrypted stream, when drmSelection is empty', 404)
         options.update(segmentTimeline=(segment_time is not None))
         mf = current_media_file
-        if mf.content_type == 'audio':
-            adp_id = 2
-        elif mf.content_type == 'video':
-            adp_id = 1
-        elif mf.content_type == 'text':
-            adp_id = 888
-        else:
+        if mf.content_type not in {'audio', 'video', 'text'}:
             return flask.make_response(
                 f'Unsupported content_type {mf.content_type}', 404)
         adp_set = AdaptationSet(
-            mode=options.mode, content_type=mf.content_type, id=adp_id,
+            mode=options.mode, content_type=mf.content_type, id=mf.track_id,
             segment_timeline=options.segmentTimeline)
         adp_set.representations.append(current_media_file.representation)
         adp_set.compute_av_values()
+        headers = {
+            'Accept-Ranges': 'bytes',
+            'Content-Type': adp_set.mimeType,
+        }
+        self.add_allowed_origins(headers)
         if segment_num == 'init':
-            mod_segment = segment_num = 0
-        else:
-            try:
-                if segment_num is not None:
-                    segment_num = int(segment_num, 10)
-            except ValueError as err:
-                logging.warning('Invalid segment number: %s', err)
-                return flask.make_response(f'Segment not found: {err}', 404)
-            err = self.check_for_synthetic_http_error(mf.content_type, segment_num, options)
-            if err:
-                return err
-            now = datetime.datetime.now(tz=UTC())
-            timing = DashTiming(now, current_stream.timing_reference, options)
-            adp_set.set_dash_timing(timing)
-            try:
-                first_fragment, last_fragment = representation.calculate_first_and_last_segment_number()
-                logging.debug('elapsedTime=%s firstFragment=%d lastFragment=%d',
-                              timing.elapsedTime, first_fragment, last_fragment)
-                segment_num, mod_segment, origin_time = representation.calculate_segment_number_and_time(
-                    segment_time, segment_num)
-                logging.debug('segment=%d mod=%d origin=%d', segment_num, mod_segment, origin_time)
-            except ValueError as err:
-                logging.warning('ValueError: %s', err)
-                msg = (f'Segment {segment_num} not found ' +
-                       f'(valid range= {first_fragment} -> ' +
-                       f'{last_fragment}): {err}')
-                logging.info("%s", msg)
-                return flask.make_response(msg, 404)
-            if segment_num < first_fragment or segment_num > last_fragment:
-                logging.info(
-                    '%s: Request for fragment %d that is not available (%d -> %d)',
-                    now, segment_num, first_fragment, last_fragment)
-                if mode == 'live':
-                    first_tc = timing.availabilityStartTime + datetime.timedelta(
-                        seconds=(first_fragment * representation.segment_duration /
-                                 representation.timescale))
-                    logging.debug('oldest fragment %d start = %s', first_fragment, first_tc)
-                return flask.make_response(
-                    f'Segment {segment_num} not found (valid range= {first_fragment}->{last_fragment})',
-                    404)
-        assert mod_segment >= 0 and mod_segment <= representation.num_media_segments
-        frag = representation.segments[mod_segment]
-        mp4_options = mp4.Options(
-            mode='rw', lazy_load=True, bug_compatibility=options.bugCompatibility)
+            return self.generate_init_segment(mode, adp_set, headers, options)
+
+        try:
+            if segment_num is not None:
+                seg_num = int(segment_num, 10)
+        except ValueError as err:
+            logging.warning('Invalid segment number: %s', err)
+            return flask.make_response('Invalid segment number', 404)
+
+        return self.generate_media_segment(
+            mode, adp_set, headers, options, seg_num, segment_time)
+
+    def generate_init_segment(
+            self,
+            mode: str,
+            adp_set: AdaptationSet,
+            headers: dict[str, str | int],
+            options: OptionsContainer) -> flask.Response:
+        mf = current_media_file
+        representation = mf.representation
+
+        err = self.check_for_synthetic_http_error(mf.content_type, 0, options)
+        if err is not None:
+            return err
+
+        atom = self.load_fragment(representation, 0, options)
         if representation.encrypted:
-            mp4_options.iv_size = representation.iv_size
-        with current_media_file.open_file(start=frag.pos, buffer_size=16384) as reader:
-            src = BufferedReader(
-                reader, offset=frag.pos, size=frag.size, buffersize=16384)
-            atom = mp4.Mp4Atom.load(src, options=mp4_options, use_wrapper=True)
-        if adp_set.content_type == 'video' and options.videoCorruption:
-            atom.moof.traf.trun.parse_samples(
-                src, representation.nalLengthFieldLength)
-        if segment_num == 0 and representation.encrypted:
             keys = models.Key.get_kids(representation.kids)
-            drms = DrmContext(stream, keys, options)
+            drms = DrmContext(current_stream, keys, options)
             for drm in drms:
                 if drm.moov is not None:
                     pssh = drm.moov(representation.default_kid)
                     atom.moov.append_child(pssh)
         if mode == 'live':
-            if segment_num == 0:
-                try:
-                    # remove the mehd box as this stream is not supposed to
-                    # have a fixed duration
-                    del atom.moov.mehd
-                except AttributeError:
-                    pass
-            else:
-                atom.moof.traf.tfdt.base_media_decode_time += origin_time
-
-                # Update the sequenceNumber field in the MovieFragmentHeader
-                # box
-                atom.moof.mfhd.sequence_number = segment_num
-                diff = None
-                if segment_time is not None:
-                    diff = segment_time - atom.moof.traf.tfdt.base_media_decode_time
-                logging.debug(
-                    r'%s: $Time$ want=%s got=%d (%s)',
-                    filename, segment_time, atom.moof.traf.tfdt.base_media_decode_time, diff)
-                diff = None
-                if segment_num is not None:
-                    diff = segment_num - atom.moof.mfhd.sequence_number
-                logging.debug(
-                    r'%s: $Number$ want=%s got=%d (%s)',
-                    filename, segment_num, atom.moof.mfhd.sequence_number, diff)
-                logging.debug(
-                    r'%s: origin=%d duration=%d',
-                    filename, origin_time, representation.segment_duration)
             try:
-                # remove any sidx box as it has a baseMediaDecodeTime and it's
-                # an optional index
-                del atom.sidx
+                # remove the mehd box as this stream is not supposed to
+                # have a fixed duration
+                del atom.moov.mehd
             except AttributeError:
                 pass
-        moof_modified = False
-        traf_modified = False
-        if segment_num > 0 and adp_set.content_type == 'video':
+        data = atom.encode()
+        return flask.make_response((data, 200, headers))
+
+    def generate_media_segment(
+            self,
+            mode: str,
+            adp_set: AdaptationSet,
+            headers: dict[str, str | int],
+            options: OptionsContainer,
+            seg_num: int | None,
+            segment_time: int | None = None) -> flask.Response:
+        mod_segment: int = 0
+        mf = current_media_file
+        representation = mf.representation
+
+        err = self.check_for_synthetic_http_error(mf.content_type, seg_num, options)
+        if err is not None:
+            return err
+
+        now = datetime.datetime.now(tz=UTC())
+        timing = DashTiming(now, current_stream.timing_reference, options)
+        adp_set.set_dash_timing(timing)
+        try:
+            first_fragment, last_fragment = representation.calculate_first_and_last_segment_number()
+            logging.debug('elapsedTime=%s firstFragment=%d lastFragment=%d',
+                          timing.elapsedTime, first_fragment, last_fragment)
+            seg_num, mod_segment, origin_time = representation.calculate_segment_number_and_time(
+                segment_time, seg_num)
+            logging.debug('segment=%d mod=%d origin=%d', seg_num, mod_segment, origin_time)
+        except ValueError as err:
+            logging.warning('ValueError: %s', err)
+            logging.info(
+                'Segment %d not found (valid range= %d -> %d)',
+                seg_num, first_fragment, last_fragment)
+            return flask.make_response('Segment not found', 404)
+
+        if seg_num < first_fragment or seg_num > last_fragment:
+            logging.info(
+                '%s: Request for fragment %d that is not available (%d -> %d)',
+                now, seg_num, first_fragment, last_fragment)
+            if mode == 'live':
+                first_tc = timing.availabilityStartTime + datetime.timedelta(
+                    seconds=(first_fragment * representation.segment_duration /
+                             representation.timescale))
+                logging.debug('oldest fragment %d start = %s', first_fragment, first_tc)
+            return flask.make_response(
+                f'Segment {seg_num} not found (valid range= {first_fragment}->{last_fragment})',
+                404)
+
+        assert mod_segment >= 0 and mod_segment <= representation.num_media_segments
+
+        atom = self.load_fragment(
+            representation, mod_segment, options,
+            parse_samples=(adp_set.content_type == 'video' and options.videoCorruption))
+
+        moof_modified: bool = False
+        traf_modified: bool = False
+
+        moof: mp4.Mp4Atom = atom.moof
+        traf: mp4.Mp4Atom = moof.traf
+
+        if mode == 'live':
+            tfdt: mp4.Mp4Atom
+            try:
+                tfdt = traf.tfdt
+            except AttributeError as err:
+                logging.debug('Adding tfdt box to traf: %s', err)
+                base_media_decode_time: int
+                base_media_decode_time = sum([
+                    seg.duration for seg in representation.segments[1:mod_segment]])
+                tfdt = mp4.TrackFragmentDecodeTimeBox(
+                    version=0, flags=0,
+                    base_media_decode_time=base_media_decode_time)
+                tfhd_index = atom.moof.traf.index('tfhd')
+                traf.insert_child(tfhd_index + 1, tfdt)
+                traf_modified = True
+                moof_modified = True
+                # force trun box to have a data_offset field, as its
+                # position will have changed
+                traf.trun.flags |= mp4.TrackFragmentRunBox.data_offset_present
+
+            tfdt.base_media_decode_time += origin_time
+
+            # Update the sequenceNumber field in the MovieFragmentHeader
+            # box
+            moof.mfhd.sequence_number = seg_num
+            diff = None
+            if segment_time is not None:
+                diff = segment_time - tfdt.base_media_decode_time
+                logging.debug(
+                    r'%s: $Time$ want=%s got=%d (%s)',
+                    mf.name, segment_time, tfdt.base_media_decode_time, diff)
+            logging.debug(
+                r'%s: origin=%d duration=%d',
+                mf.name, origin_time, representation.segment_duration)
+
+        try:
+            # remove any sidx box as it has a baseMediaDecodeTime and it's
+            # an optional index
+            del atom.sidx
+        except AttributeError:
+            pass
+
+        if adp_set.content_type == 'video':
             event_generators = EventFactory.create_event_generators(options)
             if event_generators:
                 logging.debug('creating emsg boxes')
@@ -228,7 +276,7 @@ class LiveMedia(RequestHandlerBase):
                     boxes = evgen.create_emsg_boxes(
                         moof=atom.moof,
                         adaptation_set=adp_set,
-                        segment_num=segment_num,
+                        segment_num=seg_num,
                         mod_segment=mod_segment,
                         representation=representation)
                     # the emsg boxes must be inserted before the
@@ -236,30 +284,26 @@ class LiveMedia(RequestHandlerBase):
                     for idx, emsg in enumerate(boxes):
                         atom.children.insert(moof_idx + idx, emsg)
                         moof_modified = True
-        if representation.encrypted and segment_num > 0:
-            traf_modified = self.update_traf_if_required(options, atom.moof.traf)
+        if representation.encrypted:
+            traf_modified = self.update_traf_if_required(options, traf)
             moof_modified = moof_modified or traf_modified
         if moof_modified:
-            tfhd = atom.moof.traf.find_child('tfhd')
+            tfhd = traf.find_child('tfhd')
             if tfhd is not None:
                 # force base_data_offset to be re-calculated when the
                 # tfhd box is encoded
                 tfhd.base_data_offset = None
         if traf_modified:
-            saio = atom.moof.traf.find_child('saio')
-            senc = atom.moof.traf.find_child('senc')
+            saio = traf.find_child('saio')
+            senc = traf.find_child('senc')
             if saio is not None and senc is not None:
                 # force re-calculation of SAIO offset to SENC box
                 saio.offsets = None
         dest = io.BytesIO()
         atom.encode(dest)
         if mf.content_type == 'video' and options.videoCorruption:
-            self.apply_video_corruption(representation, segment_num, atom, dest, options)
+            self.apply_video_corruption(representation, seg_num, atom, dest, options)
         data = dest.getvalue()
-        headers = {
-            'Accept-Ranges': 'bytes',
-            'Content-Type': adp_set.mimeType,
-        }
         status = 200
         try:
             start, end, status, range_headers = self.get_http_range(len(data))
@@ -268,8 +312,27 @@ class LiveMedia(RequestHandlerBase):
             headers.update(range_headers)
         except (ValueError) as ve:
             return flask.make_response(f'{ve}', 400)
-        self.add_allowed_origins(headers)
         return flask.make_response((data, status, headers))
+
+    @staticmethod
+    def load_fragment(representation: Representation,
+                      seg_index: int,
+                      options: OptionsContainer,
+                      parse_samples: bool = False) -> mp4.Mp4Atom:
+        frag = representation.segments[seg_index]
+        mp4_options = mp4.Options(
+            mode='rw', lazy_load=True, bug_compatibility=options.bugCompatibility)
+        if representation.encrypted:
+            mp4_options.iv_size = representation.iv_size
+        with current_media_file.open_file(start=frag.pos, buffer_size=16384) as reader:
+            src = BufferedReader(
+                reader, offset=frag.pos, size=frag.size, buffersize=16384)
+            atom = mp4.Mp4Atom.load(src, options=mp4_options, use_wrapper=True)
+            if parse_samples:
+                atom.moof.traf.trun.parse_samples(
+                    src, representation.nalLengthFieldLength)
+
+        return atom
 
     def update_traf_if_required(self, options: OptionsContainer, traf: mp4.BoxWithChildren) -> bool:
         """
