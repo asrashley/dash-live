82ff8bab0839bf3fdf7afc0eab3283e80cf3d159
Alex Ashley <github@ashley-family.net>
2024-10-03 09:44:58 +0100

    refactor form validation code into reusable functions

--- static/js/edit_media.js
+++ static/js/edit_media.js
@@ -1,71 +1,33 @@
 /* globals console */
 import $ from '/libs/jquery.js';
 import { enableTooltips } from './tooltips.js';
+import { validateFormValues } from './form_validation.js';
 
 async function checkFormValues() {
-    const form = document.getElementById('edit_media');
-    if (form === null) {
-        console.error('Failed to find edit media form');
-        return;
-    }
-    const url = $(form).data('validate-url');
-    if (!url) {
-        console.error('Failed to find URL for form validation');
-        return;
-    }
-    const data = Object.fromEntries(
-        $(form).serializeArray().
-            filter(item => item.name !== 'csrf_token').
-            map(item => [item.name, item.value,]));
-    $('form').addClass('needs-validation').removeClass('was-validated');
-    const result = await fetch(url, {
-        body: JSON.stringify(data),
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        method: 'POST',
-        mode: 'same-origin',
-    });
-    if (!result.ok) {
-        console.error(`Failed to check form validitity: ${ result.status }`);
-        return;
-    }
-    const { errors } = await result.json();
-    $('#edit_media input').each((_index, elt) => {
-        const $elt = $(elt);
-        const name = $elt.attr('name');
-        const parent = $elt.parent();
-        const feedback = parent.find('.invalid-feedback');
-        const err = errors[name] || '';
-        if (err === '') {
-            $elt.addClass('is-valid').removeClass('is-invalid');
-        } else {
-            $elt.removeClass('is-valid').addClass('is-invalid');
-        }
-        feedback.text(err);
-        elt.setCustomValidity(err);
-    });
-    $('form').removeClass('needs-validation').addClass('was-validated');
+  try {
+    const { form } = await validateFormValues('edit_media');
     const submit = $('#edit_media button[type="submit"]');
     if (form.checkValidity()) {
-        submit.attr('disabled', false);
+      submit.attr('disabled', false);
     } else {
-        submit.attr('disabled', 'disabled');
+      submit.attr('disabled', 'disabled');
     }
+  } catch(err) {
+    console.error(err);
+    return;
+  }
 }
 
 function setupValidation() {
-    $('#edit_media input').on('change', checkFormValues);
+  $('#edit_media input').on('change', checkFormValues);
 
-    $('#edit_media input').on('invalid', (ev) => {
-        $(ev.target).removeClass('is-valid').addClass('is-invalid');
-    });
+  $('#edit_media input').on('invalid', (ev) => {
+    $(ev.target).removeClass('is-valid').addClass('is-invalid');
+  });
 
-    $('#edit_media input').on('valid', (ev) => {
-        $(ev.target).addClass('is-valid').removeClass('is-invalid');
-    });
+  $('#edit_media input').on('valid', (ev) => {
+    $(ev.target).addClass('is-valid').removeClass('is-invalid');
+  });
 }
 
 enableTooltips();
