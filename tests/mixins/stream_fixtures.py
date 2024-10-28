f23c5211b9e4df15971464d902cdb8043a41c4a2
Alex Ashley <github@ashley-family.net>
2024-10-28 11:35:31 +0000

    add test for availabilityStartTime for multi-period streams

--- tests/mixins/stream_fixtures.py
+++ tests/mixins/stream_fixtures.py
@@ -5,13 +5,45 @@
 #  Author              :    Alex Ashley
 #
 #############################################################################
+from dataclasses import dataclass
 from typing import NamedTuple
 
-class StreamFixture(NamedTuple):
+from dashlive.mpeg.dash.content_role import ContentRole
+
+@dataclass
+class StreamFixture:
+    """
+    Describes one test stream
+    """
     name: str
     title: str
     segment_duration: float  # in seconds
     media_duration: float  # in seconds
+
+class FixtureTrack(NamedTuple):
+    """
+    Describes one track in a test multi-period stream
+    """
+    ttype: str  # "video", "audio", etc
+    tid: int
+    role: ContentRole
+
+class FixturePeriod(NamedTuple):
+    """
+    Describes one Period in a test multi-period stream
+    """
+    pid: str
+    fixture: StreamFixture
+    start: int
+    end: int
+    tracks: list[FixtureTrack]
+
+class MultiPeriodStreamFixture(StreamFixture):
+    periods: list[FixturePeriod]
+
+    def __init__(self, periods: list[FixturePeriod], **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.periods = periods
 
 
 BBB_FIXTURE = StreamFixture(
@@ -25,3 +57,24 @@ TEARS_FIXTURE = StreamFixture(
     title="Tears of Steel",
     media_duration=64,
     segment_duration=4)
+
+
+MPS_FIXTURE = MultiPeriodStreamFixture(
+    name="testmps",
+    title="Example multi-period stream",
+    segment_duration=0,
+    media_duration=(
+        BBB_FIXTURE.media_duration - 2 * BBB_FIXTURE.segment_duration +
+        TEARS_FIXTURE.media_duration - 5 * TEARS_FIXTURE.segment_duration),
+    periods=[
+        FixturePeriod(pid="p1", fixture=BBB_FIXTURE, start=1, end=1,
+                      tracks=[
+                          FixtureTrack('video', 1, ContentRole.MAIN),
+                          FixtureTrack('audio', 2, ContentRole.MAIN),
+                      ]),
+        FixturePeriod(pid="p2", fixture=TEARS_FIXTURE, start=2, end=3,
+                      tracks=[
+                          FixtureTrack('video', 1, ContentRole.MAIN),
+                          FixtureTrack('audio', 2, ContentRole.MAIN),
+                      ]),
+    ])
--- tests/test_multi_period.py
+++ tests/test_multi_period.py
@@ -20,58 +20,40 @@
 #
 #############################################################################
 import logging
-from typing import ClassVar
 import unittest
 
-from dashlive.mpeg.dash.content_role import ContentRole
-
 from .mixins.check_manifest import DashManifestCheckMixin
-from .mixins.flask_base import FlaskTestBase, FixtureTrack, FixturePeriod
-from .mixins.stream_fixtures import BBB_FIXTURE, TEARS_FIXTURE
+from .mixins.flask_base import FlaskTestBase
+from .mixins.stream_fixtures import MPS_FIXTURE
 
 class MultiPeriodTests(FlaskTestBase, DashManifestCheckMixin):
-    periods: ClassVar[list[FixturePeriod]] = [
-        FixturePeriod(pid="p1", fixture=BBB_FIXTURE, start=1, end=1,
-                      tracks=[
-                          FixtureTrack('video', 1, ContentRole.MAIN),
-                          FixtureTrack('audio', 2, ContentRole.MAIN),
-                      ]),
-        FixturePeriod(pid="p2", fixture=TEARS_FIXTURE, start=4, end=6,
-                      tracks=[
-                          FixtureTrack('video', 1, ContentRole.MAIN),
-                          FixtureTrack('audio', 2, ContentRole.MAIN),
-                      ]),
-    ]
-
     def test_generated_manifest_against_fixture_vod(self) -> None:
-        self.setup_multi_period_stream(
-            "testfix", "title", MultiPeriodTests.periods)
+        self.setup_multi_period_stream(MPS_FIXTURE)
         self.check_generated_manifest_against_fixture(
             'hand_made.mpd', mode='vod', acodec='mp4a', encrypted=False,
-            now="2023-10-25T07:56:58Z", mps_name="testfix")
+            now="2023-10-25T07:56:58Z", mps_name=MPS_FIXTURE.name)
 
     def test_generated_manifest_against_fixture_live(self) -> None:
-        self.setup_multi_period_stream(
-            "testfix", "title", MultiPeriodTests.periods)
+        self.setup_multi_period_stream(MPS_FIXTURE)
         self.check_generated_manifest_against_fixture(
             'hand_made.mpd', mode='live', acodec='mp4a', encrypted=False,
-            now="2024-10-07T00:03:00Z", mps_name="testfix",
+            now="2024-10-07T00:03:00Z", mps_name=MPS_FIXTURE.name,
             start="2024-10-07T00:00:00Z")
 
     async def test_vod(self):
-        self.setup_multi_period_stream("testfix", "title", MultiPeriodTests.periods)
+        self.setup_multi_period_stream(MPS_FIXTURE)
         await self.check_a_manifest_using_major_options(
             filename='hand_made.mpd',
             mode='vod',
-            mps_name="testfix",
+            mps_name=MPS_FIXTURE.name,
             simplified=True)
 
     async def test_live(self):
-        self.setup_multi_period_stream("livemps", "title", MultiPeriodTests.periods)
+        self.setup_multi_period_stream(MPS_FIXTURE)
         await self.check_a_manifest_using_major_options(
             filename='hand_made.mpd',
             mode='live',
-            mps_name="livemps",
+            mps_name=MPS_FIXTURE.name,
             debug=False,
             simplified=True)
 
