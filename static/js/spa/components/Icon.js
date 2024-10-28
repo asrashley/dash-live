067225f93b1daeabc879e7c27dd260f1d6df569d
Alex Ashley <github@ashley-family.net>
2024-10-28 16:16:06 +0000

    add Bootstrap icons

---
+++ static/js/spa/components/Icon.js
@@ -0,0 +1,17 @@
+import { html } from 'htm/preact';
+
+export function Icon({name, className=""}) {
+  return html`<span class="bi icon bi-${name} ${className}"></span>`;
+}
+
+function doNothing(ev) {
+  ev.preventDefault();
+  return false;
+}
+
+export function IconButton({name, onClick, disabled, className=""}) {
+  if (disabled) {
+    return html`<a onClick=${doNothing} class="disabled ${className}"><${Icon} name="${name}" /></a>`;
+  }
+  return html`<a onClick=${onClick} class="${className}" href="#"><${Icon} name="${name}" /></a>`;
+}
