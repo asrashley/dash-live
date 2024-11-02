9da18f7e2e24261782172771508098ffd9cea97a
Alex Ashley <github@ashley-family.net>
2024-11-02 09:41:06 +0000

    add Preact testing libraries

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+const projectRootDir = resolve(__dirname);
+
+export default defineConfig({
+	test: {
+		environment: 'jsdom',
+        setupFiles: './static/js/test/setup.js',
+        coverage: {
+            include: [
+                "static/js/spa/**/*.js",
+            ],
+            exclude: [
+                "static/js/test/*.js",
+            ],
+            thresholds: {
+                branches: 75,
+                functions: 80,
+                lines: 75,
+                statements: 75
+            }
+        },
+	},
+	resolve: {
+		alias: [
+			{
+				find: '/libs/routemap.js',
+				replacement: resolve(projectRootDir, 'static/js/mocks/routemap.js')
+			}
+		]
+	}
+})
