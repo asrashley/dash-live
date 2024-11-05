555db04a29a904c86221c789aef12f7188593681
Alex Ashley <github@ashley-family.net>
2024-11-05 18:47:32 +0000

    show validation errors when editing an MP stream

--- static/js/spa/components/TextInput.js
+++ static/js/spa/components/TextInput.js
@@ -2,6 +2,7 @@ import { html } from 'htm/preact';
 
 export function TextInput({name, error, ...props}) {
   const className = `form-control ${error ? "is-invalid" : "is-valid"}`;
+
   return html`
     <input class="${className}" id="field-${name}" name="${name}"
       type="text" ...${props} />`;
--- static/js/spa/components/TextInputRow.js
+++ static/js/spa/components/TextInputRow.js
@@ -6,6 +6,6 @@ import { TextInput } from './TextInput.js';
 export function TextInputRow({ name, label, text, error, ...props}) {
   return html`
 <${FormRow} name=${name} label=${label} text=${text} error=${error} >
-  <${TextInput} name="${name}" ...${props} />
+  <${TextInput} name="${name}" error=${error} ...${props} />
 </${FormRow}>`;
 }
