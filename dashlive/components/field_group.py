23497aaa8c2ced6c6e40dd9796533f185e166511
Alex Ashley <github@ashley-family.net>
2024-12-06 09:52:49 +0000

    exclude track selection options from mps options
    Multi-period streams have their tracks selected using the
    adaptation sets configured using the UI. The CGI options that
    allow controlling tracks in a non-mps stream are not relevant.

--- dashlive/components/field_group.py
+++ dashlive/components/field_group.py
@@ -7,20 +7,27 @@
 #############################################################################
 
 from dataclasses import dataclass
-from typing import AbstractSet
+from typing import AbstractSet, TypedDict
 
-from dashlive.utils.json_object import JsonObject
+from dashlive.server.options.form_input_field import FormInputContext
+
+class InputFieldGroupJson(TypedDict):
+    name: str
+    title: str
+    fields: list[FormInputContext]
+    show: bool
+    className: str
 
 @dataclass(slots=True)
 class InputFieldGroup:
     name: str
     title: str
-    fields: list[JsonObject]
+    fields: list[FormInputContext]
     show: bool = False
     className: str = ''
 
-    def toJSON(self, pure: bool = False, exclude: AbstractSet | None = None) -> JsonObject:
-        js = {
+    def toJSON(self, pure: bool = False, exclude: AbstractSet | None = None) -> InputFieldGroupJson:
+        js: InputFieldGroupJson = {
             'name': self.name,
             'title': self.title,
             'show': self.show,
