a44f265edc0cef073c6fdd6f23fa606b71d791a4
Alex Ashley <github@ashley-family.net>
2024-12-08 09:56:40 +0000

    use tabs rather than accordian for MPS options
    Using an accordian component to show all of the multi-period stream
    options makes the dialog box very long. Using tabs takes up much less
    space and is more suitable for a dialog box.

--- static/js/spa/components/FormRow.js
+++ static/js/spa/components/FormRow.js
@@ -1,16 +1,5 @@
 import { html } from "htm/preact";
 
-import { SelectOption } from "./SelectOption.js";
-
-function DataList({name, options, type='datallist'}) {
-  if (type !== 'datalist') {
-    return null;
-  }
-  return html`<datalist id="list-${ name }">
-  ${ options.map(opt => html`<${SelectOption} ...${opt} />`)}
-</datalist>`;
-}
-
 function FormText({text, className}) {
   if (!text) {
     return null;
@@ -25,17 +14,16 @@ function ErrorFeedback({error}) {
   return html`<div class="invalid-feedback" style="display:block">${error}</div>`;
 }
 
-export function FormRow({ className = "", name, layout, label, text, type, children, error, options }) {
+export function FormRow({ className = "", name, layout, label, text, children, error }) {
   let [left, middle, right] = layout ?? [2, 7, 3];
   if (!text) {
     middle += right;
   }
   return html`
-    <div class="row mb-3 form-group ${className}">
+    <div class="row mb-2 form-group ${className}">
       <label className="col-${left} col-form-label" htmlFor="field-${name}">${label}:</label>
       <div className="col-${middle}">${children}</div>
       <${FormText} text=${text} className="col-${right}" />
       <${ErrorFeedback} error=${error} />
-      <${DataList} type=${type} name=${name} options=${options} />
     </div>`;
 }
