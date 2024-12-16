d6b712315d7bfa8f92dc84c65a344b9792574078
Alexis Ashley <github@ashley-family.net>
2024-12-16 10:53:43 +0000

    pre-populate content types if the table is empty
    When starting the server for the first time with empty database
    tables, pre-populate the ContentType table with the most commonly found
    content types.

--- dashlive/server/models/content_type.py
+++ dashlive/server/models/content_type.py
@@ -17,19 +17,32 @@ from .session import DatabaseSession
 if TYPE_CHECKING:
     from .adaptation_set import AdaptationSet
 
+
 class ContentType(db.Model, ModelMixin):
     """
     Table for holding RFC 6838 content types
     """
-    __plural__: ClassVar[str] = 'ContentTypes'
+
+    __plural__: ClassVar[str] = "ContentTypes"
     __tablename__: ClassVar[str] = "content_type"
 
     pk: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
-    adaptation_sets: Mapped[list["AdaptationSet"]] = relationship(back_populates="content_type")
+    adaptation_sets: Mapped[list["AdaptationSet"]] = relationship(
+        back_populates="content_type"
+    )
 
     @classmethod
-    def get(cls,
-            session: DatabaseSession | None = None,
-            **kwargs) -> Optional["ContentType"]:
+    def get(
+        cls, session: DatabaseSession | None = None, **kwargs
+    ) -> Optional["ContentType"]:
         return cast(Optional[ContentType], cls.get_one(session=session, **kwargs))
+
+    @classmethod
+    def populate_if_empty(cls) -> None:
+        count: int = cls.count()
+        if count > 0:
+            return
+        for name in ["application", "video", "audio", "text", "image"]:
+            ct = ContentType(name=name)
+            db.session.add(ct)
