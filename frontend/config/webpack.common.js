b4c8e801d698a513cb54a62e4755ed7d115c6e0a
Alex Ashley <github@ashley-family.net>
2025-01-07 14:07:46 +0000

    add webpack config files for development and production builds

---
+++ frontend/config/webpack.common.js
@@ -0,0 +1,81 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export const rootDir = path.resolve(
+  fileURLToPath(new URL(".", import.meta.url)),
+  "..",
+  ".."
+);
+
+const dateNow = new Date();
+
+export const commonConfig = ({ initialTokens, initialUser, publicPath, template, tsConfigFile }) => ({
+  devtool: "source-map",
+  entry: {
+    main: "./frontend/src/main",
+  },
+  experiments: {
+    outputModule: true,
+  },
+  externals: {
+    '@dashlive/options': 'module /libs/options.js',
+    '@dashlive/routemap': 'module /libs/routemap.js'
+  },
+  output: {
+    path: path.resolve(rootDir, "templates", "spa"),
+    filename: "[name].[contenthash].js",
+    module: true
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+        options: {
+          helperDirs: [path.join(rootDir, "frontend", "html", "helpers")],
+          partialDirs: [path.join(rootDir, "frontend", "html", "partials")],
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: tsConfigFile,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template,
+      hash: true,
+      chunks: ["main"],
+      publicPath: publicPath === "" ? "/" : publicPath,
+      inject: true,
+      scriptLoading: "module",
+      templateParameters: (_compilation, _assets, options) => ({
+        htmlWebpackPlugin: {
+          options,
+        },
+        template: {
+          ...options,
+          title: 'DASH live server',
+          dateNow,
+          publicPath,
+          initialTokens,
+          initialUser,
+        },
+      }),
+    }),
+  ],
+});
