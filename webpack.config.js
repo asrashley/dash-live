6a8eca17a81ba2823775304881f25200c4320651
Alex Ashley <github@ashley-family.net>
2025-01-10 08:05:55 +0000

    setup build rules for making production build
    A production build places the output into the "static/html" directory
    and should exclude all test code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,11 @@
 import path from "path";
 import { rootDir, commonConfig } from "./frontend/config/webpack.common.js";
 
-const apiTokens = {
-  csrfTokens: {
-      files: null,
-      kids: null,
-      streams: null,
-      upload: null
-  },
-  accessToken: null,
-  refreshToken: null,
-};
-
-const initialUserState = {
-  isAuthenticated: false,
-  groups:[],
-};
-
 const serverPort = process.env.SERVER_PORT || '5000';
 
 const common = commonConfig({
   publicPath: "",
-  template: path.join(rootDir, "frontend/html/index.hbs"),
   tsConfigFile: "tsconfig.json",
-  initialTokens: JSON.stringify(apiTokens),
-  initialUser: JSON.stringify(initialUserState),
 });
 
 export default {
