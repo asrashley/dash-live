eacae2dddcc4b3f26a511b507337c8cae1228b57
Alex Ashley <github@ashley-family.net>
2025-01-13 10:35:05 +0000

    ask the user to login if there are no media files and not already logged in

--- frontend/src/home/components/NoStreamsMessage.test.tsx
+++ frontend/src/home/components/NoStreamsMessage.test.tsx
@@ -2,13 +2,31 @@ import { describe, expect, test } from "vitest";
 
 import { renderWithProviders } from "../../test/renderWithProviders";
 import { NoStreamsMessage } from "./NoStreamsMessage";
+import { InitialUserState } from "../../types/UserState";
 
 describe("NoStreamsMessage", () => {
-  test("should display message", () => {
-    const { asFragment, getByText } = renderWithProviders(
+  test("should display message with a login link", () => {
+    const { asFragment, getByText, getByTestId } = renderWithProviders(
       <NoStreamsMessage />
     );
-    getByText('Please add some media files');
+    getByTestId('needs-login');
+    getByText('to add some media files', { exact: false});
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("should display message for a user who is already logged in", () => {
+    const userInfo: InitialUserState = {
+      pk: 123,
+      username: 'test',
+      email: 'test@unit.local',
+      isAuthenticated: true,
+      groups: ['USER']
+    }
+    const { asFragment, queryByTestId, getByText } = renderWithProviders(
+      <NoStreamsMessage />, { userInfo }
+    );
+    expect(queryByTestId('needs-login')).toBeNull();
+    getByText('to add some media files', { exact: false});
     expect(asFragment()).toMatchSnapshot();
   });
 });
