a21c928ee3c13b53b23a3654f0def13c28d5addc
Alex Ashley <github@ashley-family.net>
2025-01-13 10:35:05 +0000

    ask the user to login if there are no media files and not already logged in

--- frontend/src/home/components/NoStreamsMessage.tsx
+++ frontend/src/home/components/NoStreamsMessage.tsx
@@ -1,17 +1,28 @@
-import { routeMap } from "@dashlive/routemap";
+import { Fragment } from "preact";
+import { useContext } from "preact/hooks";
+import { routeMap, uiRouteMap } from "@dashlive/routemap";
 import { Card } from "../../components/Card";
 import { Icon } from "../../components/Icon";
+import { AppStateContext } from "../../appState";
+
+function PleaseLogin() {
+  const { user } = useContext(AppStateContext);
+
+  if (user.value.isAuthenticated) {
+    return null;
+  }
+  return <Fragment><a className="link" data-testid="needs-login" href={uiRouteMap.login.url()}>login</a> and then </Fragment>;
+}
 
 export function NoStreamsMessage() {
   return (
     <Card id="no-streams-msg" header="Please add some media files">
       <p>
         <Icon className="fs-2 me-2" name="camera-video-off" />
-        There are no streams in the database. Please go to{" "}
-        <a className="link" href={routeMap.listStreams.url()}>
+        There are no streams in the database.
+        Please <PleaseLogin />go to <a className="link" href={routeMap.listStreams.url()}>
           the streams page
-        </a>{" "}
-        and add some streams.
+        </a>{" "}to add some media files.
       </p>
     </Card>
   );
