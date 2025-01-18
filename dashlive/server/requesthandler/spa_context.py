e422138b262a051ebc6b1af0b2f96762cb48425b
Alex Ashley <github@ashley-family.net>
2025-01-18 15:29:42 +0000

    implement REST API to query user info from a refresh token
    to allow the front end app to find out the information about a user
    given their refresh token, add a GET handler to the /api/login path
    that provides information about the user if the refresh token is
    valid.

--- dashlive/server/requesthandler/spa_context.py
+++ dashlive/server/requesthandler/spa_context.py
@@ -7,9 +7,7 @@
 #############################################################################
 from typing import TypedDict
 
-from flask_login import current_user
-
-from dashlive.server.models.token import DecodedJwtToken, EncodedJWTokenJson, Token, TokenType
+from dashlive.server.models.token import EncodedJWTokenJson, Token, TokenType
 from dashlive.server.models.user import User
 
 from .csrf import CsrfProtection, CsrfTokenCollection, CsrfTokenCollectionJson
@@ -18,7 +16,6 @@ from .navbar import create_navbar_context, NavBarItem
 class InitialTokensType(TypedDict):
     csrfTokens: CsrfTokenCollectionJson
     accessToken: EncodedJWTokenJson | None
-    refreshToken: EncodedJWTokenJson | None
 
 
 class UserContextType(TypedDict):
@@ -34,32 +31,24 @@ class SpaTemplateContext(TypedDict):
     user: UserContextType
 
 
-def create_spa_template_context() -> SpaTemplateContext:
+def create_spa_template_context(user: User) -> SpaTemplateContext:
     csrf_key: str = CsrfProtection.generate_cookie()
     csrf_tokens = CsrfTokenCollection(
         streams=CsrfProtection.generate_token('streams', csrf_key),
         files=None,
         kids=None,
         upload=None)
-    user: User = current_user
-    if not current_user.is_authenticated:
-        user = User.get_guest_user()
     access_token: EncodedJWTokenJson = Token.generate_api_token(user, TokenType.ACCESS).toJSON()
     initial_tokens: InitialTokensType = {
         'csrfTokens': csrf_tokens.to_dict(),
         'accessToken': access_token,
-        'refreshToken': None,
     }
     user_context: UserContextType = {
-        'isAuthenticated': current_user.is_authenticated,
+        'isAuthenticated': user.is_authenticated,
         'pk': user.pk,
         'username': user.username,
         'groups': user.get_groups(),
     }
-    if current_user.is_authenticated:
-        refresh_token: EncodedJWTokenJson = Token.generate_api_token(
-            current_user, TokenType.REFRESH).toJSON()
-        initial_tokens['refreshToken'] = refresh_token
     navbar: list[NavBarItem] = create_navbar_context(with_login=False)
     context: SpaTemplateContext = {
         "navbar": navbar,
