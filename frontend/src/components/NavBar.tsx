a0d498871d2af24a2d4cbddf76343901782acc29
Alex Ashley <github@ashley-family.net>
2025-01-21 16:47:48 +0000

    implement expand/collapse for NavBar for small page widths
    when the page width is less than 990px, the top level menu
    converts into a single menu item with a drop-down. The NavBar
    component needed to be updated to support showing and hiding this
    menu when on smaller displays.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "preact/hooks";
 import { NavBarItem } from "../types/NavBarItem";
 import { LoginLogoutLink } from "./LoginLogoutLink";
 import { NavItem } from "./NavItem";
@@ -6,20 +7,26 @@ export interface NavBarProps {
   items: NavBarItem[];
 }
 export function NavBar({ items }: NavBarProps) {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const toggleExpand = useCallback(() => setExpanded(!expanded), [expanded]);
+  const togglerClassName = `navbar-toggler${expanded ? '' : ' collapsed'}`;
+  const navbarClassName = `collapse navbar-collapse${ expanded ? ' show' : ''}`;
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
       <span className="navbar-brand">DASH server</span>
       <button
-        className="navbar-toggler"
+        onClick={toggleExpand}
+        className={togglerClassName}
         type="button"
         aria-controls="navbarSupportedContent"
-        aria-expanded="false"
+        aria-expanded={expanded}
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon" />
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className={navbarClassName} id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           {items.map((item) => (
             <NavItem key={item.title} {...item} />
