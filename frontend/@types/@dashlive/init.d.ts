aa5a8c0665c169e5ffbfc73bfece5911b0123930
Alex Ashley <github@ashley-family.net>
2025-01-21 13:53:58 +0000

    user is no longer required in initialAppState.js

--- frontend/@types/@dashlive/init.d.ts
+++ frontend/@types/@dashlive/init.d.ts
@@ -1,9 +1,6 @@
 import { InitialApiTokens } from "../../src/types/InitialApiTokens";
 import { NavBarItem } from "../../src/types/NavBarItem";
-import { InitialUserState } from "../../src/types/UserState";
 
 export const initialTokens: InitialApiTokens;
 
 export const navbar: NavBarItem[];
-
-export const user: InitialUserState;
