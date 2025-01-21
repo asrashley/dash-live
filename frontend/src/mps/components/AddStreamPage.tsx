aa7ba24ac0c7a7f9394483aefc8f72fa084fa78b
Alex Ashley <github@ashley-family.net>
2025-01-21 11:24:32 +0000

    move styles from spa.less that apply to all SPA pages

--- frontend/src/mps/components/AddStreamPage.tsx
+++ frontend/src/mps/components/AddStreamPage.tsx
@@ -1,4 +1,5 @@
 import { EditStreamCard } from './EditStreamCard';
+import '../../styles/mps.less';
 
 export default function AddStreamPage() {
   return <EditStreamCard name=".add" newStream />;
--- frontend/src/mps/components/EditStreamPage.tsx
+++ frontend/src/mps/components/EditStreamPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from "wouter-preact";
 import { EditStreamCard } from './EditStreamCard';
 import { RouteParamsType } from "../../types/RouteParamsType";
 
+import '../../styles/mps.less';
+
 export default function EditStreamPage() {
   const {mps_name} = useParams<RouteParamsType>();
   return <EditStreamCard name={mps_name} />;
