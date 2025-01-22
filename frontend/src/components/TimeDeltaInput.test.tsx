9615e612758a87b0d6ca06c7fb41945e4893f611
Alex Ashley <github@ashley-family.net>
2025-01-22 22:10:02 +0000

    add unit tests for changing value in a TimeDeltaInput component

--- frontend/src/components/TimeDeltaInput.test.tsx
+++ frontend/src/components/TimeDeltaInput.test.tsx
@@ -1,11 +1,16 @@
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 
 import { renderWithProviders } from "../test/renderWithProviders";
 import { TimeDeltaInput } from "./TimeDeltaInput";
 
 describe("TimeDeltaInput", () => {
+  const onChange = vi.fn();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   test("matches snapshot", () => {
-    const onChange = vi.fn();
     const { getBySelector, asFragment } = renderWithProviders(
       <TimeDeltaInput name="tdi" value="PT5M12S" onChange={onChange} />
     );
@@ -13,5 +18,27 @@ describe("TimeDeltaInput", () => {
     expect(inp.value).toEqual("00:05:12");
     expect(inp.className.trim()).toEqual("form-control is-valid");
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("empty duration", () => {
+    const { getBySelector } = renderWithProviders(
+      <TimeDeltaInput name="tdi" value="" onChange={onChange} />
+    );
+    const inp = getBySelector('input[type="time"]') as HTMLInputElement;
+    expect(inp.value).toEqual("00:00:00");
+  });
+
+  test.each<[string, string]>([
+    ["00:01", "PT1S"],
+    ["02:03", "PT2M3S"],
+    ["01:02:03", "PT1H2M3S"]
+  ])("changing time input to %s sets value to %s", (inpValue: string, isoValue: string) => {
+    const { getBySelector } = renderWithProviders(
+      <TimeDeltaInput name="tdi" value="PT5M12S" onChange={onChange} />
+    );
+    const inp = getBySelector('input[type="time"]') as HTMLInputElement;
+    inp.value = inpValue;
+    inp.dispatchEvent(new Event("input", { bubbles: true }));
+    expect(onChange).toHaveBeenCalledWith("tdi", isoValue);
   });
 });
