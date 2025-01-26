de115d62b00152870d61084a2ba3ed9249c7211e
Alex Ashley <github@ashley-family.net>
2025-01-26 16:27:22 +0000

    convert the table in OptionsDetailTable to a reusable component generator
    To allow mutliple components to re-use the table logic from
    OptionsDetailTable, convert it into a TypeScript templated function.

--- frontend/src/cgi/components/OptionsDetailTable.tsx
+++ frontend/src/cgi/components/OptionsDetailTable.tsx
@@ -1,93 +1,17 @@
-import { Fragment } from "preact";
-import { useComputed } from "@preact/signals";
-
+import { createSortableTable } from "../../components/SortableTable";
 import {
   useOptionsDetails,
-  UseOptionsDetailsHook,
 } from "../hooks/useOptionsDetails";
 import { InputOptionName } from "../types/InputOptionName";
-import { Icon } from "../../components/Icon";
 
-function OptionsDetailRow({ shortName, fullName, cgiName }: InputOptionName) {
-  return (
-    <tr id={`opt_${shortName}`}>
-      <td class="short-name">{shortName}</td>
-      <td class="full-name">{fullName}</td>
-      <td class="cgi-param">{cgiName}</td>
-    </tr>
-  );
-}
 
-interface SortableHeadingProps {
-  name: keyof InputOptionName;
-  setSort: UseOptionsDetailsHook["setSort"];
-  sortField: UseOptionsDetailsHook["sortField"];
-  sortOrder: UseOptionsDetailsHook["sortOrder"];
-  title: string;
-}
-
-function SortableHeading({
-  name,
-  setSort,
-  sortField,
-  sortOrder,
-  title,
-}: SortableHeadingProps) {
-  const linkClass = `link${name === sortField.value ? " fw-bold" : ""}`;
-  const iconClassName = name === sortField.value ? "opacity-100" : "opacity-50";
-  const iconName = sortOrder.value ? "sort-alpha-down" : "sort-alpha-up";
-  const onClick = () => {
-    setSort(name, name === sortField.value ? !sortOrder.value : true);
-  };
-
-  return (
-    <a href="#" className={linkClass} onClick={onClick}>
-      <span className="me-1">{title}</span>
-      <Icon name={iconName} className={iconClassName} />
-    </a>
-  );
-}
+const OptionsDetailTableComponent = createSortableTable<InputOptionName>([
+  ["shortName", "Short Name"],
+  ["fullName", "Full Name"],
+  ["cgiName", "CGI Name"],
+], "shortName", "shortName");
 
 export function OptionsDetailTable() {
-  const { allOptions, ...sortProps } = useOptionsDetails();
-  const sortNameClass = useComputed<string>(
-    () => `sort_${sortProps.sortField.value}`
-  );
-
-  return (
-    <Fragment>
-      <a id="json" />
-      <table
-        className="cgi-json-options table table-striped table-bordered"
-        style="width: auto"
-      >
-        <thead>
-          <tr className={sortNameClass}>
-            <th className="short-name">
-              <SortableHeading
-                name="shortName"
-                title="Short Name"
-                {...sortProps}
-              />
-            </th>
-            <th className="full-name">
-              <SortableHeading
-                name="fullName"
-                title="Full Name"
-                {...sortProps}
-              />
-            </th>
-            <th className="cgi-param">
-              <SortableHeading name="cgiName" title="CGI Name" {...sortProps} />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {allOptions.value.map((opt) => (
-            <OptionsDetailRow key={opt.shortName} {...opt} />
-          ))}
-        </tbody>
-      </table>
-    </Fragment>
-  );
+  const { allOptions } = useOptionsDetails();
+  return <OptionsDetailTableComponent data={allOptions} />;
 }
--- frontend/src/cgi/hooks/useOptionsDetails.test.ts
+++ frontend/src/cgi/hooks/useOptionsDetails.test.ts
@@ -1,37 +1,10 @@
 import { renderHook } from "@testing-library/preact";
 import { describe, expect, test } from "vitest";
 import { useOptionsDetails } from "./useOptionsDetails";
-import { InputOptionName } from "../types/InputOptionName";
 
 describe('useOptionsDetails hook', () => {
     test('get all options', () => {
         const { result } = renderHook(() => useOptionsDetails());
-        expect(result.current.sortField.value).toEqual("fullName");
-        expect(result.current.sortOrder.value).toEqual(true);
         expect(result.current.allOptions.value).toMatchSnapshot();
-    });
-
-    test('can sort options', () => {
-        const { result } = renderHook(() => useOptionsDetails());
-        const { setSort } = result.current;
-        const origOptions = [...result.current.allOptions.value];
-        setSort("fullName", false);
-        expect(result.current.allOptions.value).not.toEqual(origOptions);
-        setSort("fullName", true);
-        expect(result.current.allOptions.value).toEqual(origOptions);
-        origOptions.sort((a: InputOptionName, b: InputOptionName) => {
-            const left = a.shortName;
-            const right = b.shortName;
-            if (left === right) {
-              return 0;
-            }
-            if (left < right) {
-              return -1;
-            }
-            return 1;
-        });
-        expect(result.current.allOptions.value).not.toEqual(origOptions);
-        setSort("shortName", true);
-        expect(result.current.allOptions.value).toEqual(origOptions);
     });
 });
