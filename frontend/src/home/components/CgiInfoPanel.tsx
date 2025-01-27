a130e033e73d961de3b4265ae4a84ac3e1a89f04
Alex Ashley <github@ashley-family.net>
2025-01-27 12:04:08 +0000

    don't cause a page reload when going to CgiOptions page
    The link to the CGI options page was using an anchor tag, which
    causes the whole page to be reloaded. Using a Link element allows
    changing page without having to reload the entire SPA.

--- frontend/src/home/components/CgiInfoPanel.tsx
+++ frontend/src/home/components/CgiInfoPanel.tsx
@@ -1,11 +1,12 @@
 import { uiRouteMap } from '@dashlive/routemap';
+import { Link } from 'wouter-preact';
 
 export function CgiInfoPanel() {
     return <p className="info">
       The MPD files for live streams are dynamically generated so that they
       appear to be live sources, using static media files. Requests for
       manifests and for media fragments can be modified using various CGI
-      parameters, which are documented on the <a href={uiRouteMap.cgiOptions.url()}
-      className="link">CGI options</a> page.
+      parameters, which are documented on the <Link href={uiRouteMap.cgiOptions.url()}
+      className="link">CGI options</Link> page.
     </p>;
 }
