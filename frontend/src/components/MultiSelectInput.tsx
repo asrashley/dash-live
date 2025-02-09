ba682c55c08313937a892b640c17ee5adee78ecc
Alex Ashley <github@ashley-family.net>
2025-02-09 21:27:12 +0000

    disabledFields needs to be passed to input elements

--- frontend/src/components/MultiSelectInput.tsx
+++ frontend/src/components/MultiSelectInput.tsx
@@ -1,51 +1,83 @@
 import { type JSX } from "preact";
-import { type ReadonlySignal } from "@preact/signals";
+import { useComputed, type ReadonlySignal } from "@preact/signals";
 import { useCallback } from "preact/hooks";
 
 import type { SelectOptionType } from "../types/SelectOptionType";
 import type { SetValueFunc } from "../types/SetValueFunc";
-import type { InputFormData } from '../types/InputFormData';
+import type { InputFormData } from "../types/InputFormData";
+import { InputProps } from "../types/InputProps";
 
 interface MultiSelectCheckboxProps {
-  fieldName: string;
-  name?: string;
+  name: string;
   title: string;
   data: ReadonlySignal<InputFormData>;
+  disabledFields: InputProps["disabledFields"];
   onClick: (ev: JSX.TargetedEvent<HTMLInputElement>) => void;
 }
-function MultiSelectCheckbox({ name: optName, fieldName, data, title, onClick }: MultiSelectCheckboxProps) {
-  const name = optName ?? fieldName;
-  const checked = data.value[name] === "1" || data.value[name] === true;
-  return <div
-        className="form-check form-check-inline" key={name}
-      >
-        <input
-          name={name}
-          className="form-check-input"
-          type="checkbox"
-          id={`msi${name}`}
-          checked={checked}
-          onClick={onClick}
-        />
-        <label className="form-check-label me-3" for={`msi${name}`}>{title}</label>
-      </div>;
+
+function MultiSelectCheckbox({
+  name,
+  data,
+  disabledFields,
+  title,
+  onClick,
+}: MultiSelectCheckboxProps) {
+  const checked = useComputed<boolean>(() => data.value[name] === "1" || data.value[name] === true);
+  const disabled = useComputed<boolean>(() => !!disabledFields.value[name]);
+
+  return (
+    <div className="form-check form-check-inline" key={name}>
+      <input
+        name={name}
+        className="form-check-input"
+        type="checkbox"
+        id={`msi${name}`}
+        checked={checked}
+        disabled={disabled}
+        onClick={onClick}
+      />
+      <label className="form-check-label me-3" for={`msi${name}`}>
+        {title}
+      </label>
+    </div>
+  );
 }
 export interface MultiSelectInputProps {
   className: string | ReadonlySignal<string>;
   name: string;
   options: SelectOptionType[];
   data: ReadonlySignal<InputFormData>;
+  disabledFields: InputProps["disabledFields"];
   setValue: SetValueFunc;
 }
 
-export function MultiSelectInput({ className, options, data, name: fieldName, setValue }: MultiSelectInputProps) {
-  const onClick = useCallback((ev: JSX.TargetedEvent<HTMLInputElement>) => {
-    const { name, checked } = ev.target as HTMLInputElement;
-    setValue(name, checked);
-  }, [setValue]);
+export function MultiSelectInput({
+  className,
+  options,
+  name: fieldName,
+  setValue,
+  ...props
+}: MultiSelectInputProps) {
+  const onClick = useCallback(
+    (ev: JSX.TargetedEvent<HTMLInputElement>) => {
+      const { name, checked } = ev.target as HTMLInputElement;
+      setValue(name, checked);
+    },
+    [setValue]
+  );
 
   const testId = `msi-${fieldName}`;
-  return <div data-testid={testId} className={className}>
-    {options.map((opt) => <MultiSelectCheckbox key={opt.name ?? fieldName} fieldName={fieldName} data={data} onClick={onClick} {...opt} />)}
-  </div>;
+  return (
+    <div data-testid={testId} className={className}>
+      {options.map(({name, ...opt}) => (
+        <MultiSelectCheckbox
+          key={name ?? fieldName}
+          name={name ?? fieldName}
+          onClick={onClick}
+          {...props}
+          {...opt}
+        />
+      ))}
+    </div>
+  );
 }
