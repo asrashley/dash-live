b0c800fd658fa76dd1e0e2378ba09cdd71b08ede
Alex Ashley <github@ashley-family.net>
2025-02-11 18:31:17 +0000

    show permission denied error if user not logged in

--- frontend/src/components/ProtectedPage.test.tsx
+++ frontend/src/components/ProtectedPage.test.tsx
@@ -7,35 +7,72 @@ import { ProtectedPage } from "./ProtectedPage";
 import { ApiRequests, EndpointContext } from "../endpoints";
 import { LoginResponse } from "../types/LoginResponse";
 import { normalUser } from "../test/MockServer";
+import { InitialUserState } from "../types/UserState";
+
+type TestCase = {
+  title: string;
+  resolveFetch: boolean;
+  optional: boolean;
+  user: InitialUserState | null;
+};
 
 describe("ProtectedPage component", () => {
   const apiRequests = mock<ApiRequests>();
 
-  test.each([true, false])(
-    "shows loading spinner and then component when completes getUserInfo = %s",
-    async (resolvesFetch: boolean) => {
-      const blocker = Promise.withResolvers<LoginResponse>();
+  test.each<TestCase>([
+    {
+      title: "normal user, resolves fetch",
+      resolveFetch: true,
+      optional: false,
+      user: normalUser,
+    },
+    {
+      title: "normal user, fetch does not complete",
+      resolveFetch: false,
+      optional: false,
+      user: normalUser,
+    },
+    {
+      title: "not logged-in optional protection, resolves fetch",
+      resolveFetch: true,
+      optional: true,
+      user: null,
+    },
+    {
+      title: "not logged-in mandatory protection, changes location",
+      resolveFetch: true,
+      optional: false,
+      user: null,
+    },
+  ])(
+    "$title",
+    async ({ resolveFetch, optional, user }: TestCase) => {
+      const blocker = Promise.withResolvers<LoginResponse | Response>();
       apiRequests.getUserInfo.mockReturnValue(blocker.promise);
       const { whoAmI, getBySelector, findByText } = renderWithProviders(
         <EndpointContext.Provider value={apiRequests}>
-          <ProtectedPage>
+          <ProtectedPage optional={optional}>
             <h1>Hello</h1>
           </ProtectedPage>
         </EndpointContext.Provider>
       );
       getBySelector(".lds-ring");
       act(() => {
-        if (resolvesFetch) {
+        if (resolveFetch) {
           const login: LoginResponse = {
             success: true,
             csrf_token: "123",
-            user: normalUser,
+            user,
           };
-          blocker.resolve(login);
+          blocker.resolve(user ? login : new Response(null, { status: 401 }));
         }
-        whoAmI.setUser(normalUser);
+        whoAmI.setUser(user);
       });
-      await findByText("Hello");
+      if (!user && !optional) {
+        await findByText("You need to log in to access this page");
+      } else {
+        await findByText("Hello");
+      }
     }
   );
 
