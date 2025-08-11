735fff6f3bf85510a824103f3616eb641db2b9f1
Alex Ashley <github@ashley-family.net>
2025-08-11 17:13:18 +0100

    avoid random characters that can be shell escapes
    the python dotenv library does not expect things like \'
    in the environment variables listed in .env

--- gen-settings.py
+++ gen-settings.py
@@ -8,18 +8,18 @@ import os
 import sys
 
 TEMPLATE="""
-FLASK_SECRET_KEY='{cookie}'
-FLASK_DASH__CSRF_SECRET='{csrf}'
-FLASK_DASH__DEFAULT_ADMIN_USERNAME='admin'
-FLASK_DASH__DEFAULT_ADMIN_PASSWORD='{password}'
-FLASK_DASH__ALLOWED_DOMAINS='*'
-FLASK_DASH__STRICT_CSRF_ORIGIN='False'
-FLASK_DASH__PROXY_DEPTH='{proxy_depth}'
-JWT_SECRET_KEY='{jwt_key}'
+FLASK_SECRET_KEY="{cookie}"
+FLASK_DASH__CSRF_SECRET="{csrf}"
+FLASK_DASH__DEFAULT_ADMIN_USERNAME="admin"
+FLASK_DASH__DEFAULT_ADMIN_PASSWORD="{password}"
+FLASK_DASH__ALLOWED_DOMAINS="*"
+FLASK_DASH__STRICT_CSRF_ORIGIN="False"
+FLASK_DASH__PROXY_DEPTH="{proxy_depth}"
+JWT_SECRET_KEY="{jwt_key}"
 """
 
 def make_random_string(length: int) -> str:
-    chars = string.ascii_letters + string.digits + r'!#$%&()+,-./:<=>?@[\]^_`{|}~'
+    chars: str = string.ascii_letters + string.digits + r'!#%&()+,-./:<=>?@[]^_{}~'
     rv = []
     for i in range(length):
         rv.append(random.choice(chars))
