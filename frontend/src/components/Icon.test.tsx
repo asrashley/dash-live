0550a8b415bc9b185600a7cd842ad767c4c2d123
Alex Ashley <github@ashley-family.net>
2025-08-18 14:38:37 +0100

    allow a signal to be used for an icon's name

--- frontend/src/components/Icon.test.tsx
+++ frontend/src/components/Icon.test.tsx
@@ -2,13 +2,34 @@ import { describe, expect, test } from "vitest";
 
 import { renderWithProviders } from "../test/renderWithProviders";
 import { Icon } from "./Icon";
+import { signal } from "@preact/signals-core";
 
 describe("Icon", () => {
   test("should display an icon", () => {
     const { getByRole } = renderWithProviders(
-      <Icon name="snow3" />
+      <Icon name="snow3" className="hello-world" />
+    );
+    const span = getByRole('img') as HTMLElement;
+    expect(span.className.trim()).toEqual('bi icon bi-snow3 hello-world');
+    expect(span.getAttribute('aria-label')).toEqual('snow3 icon');
+  });
+
+  test("can use a signal for name", () => {
+    const name = signal("snow3");
+    const { getByRole } = renderWithProviders(
+      <Icon name={name} />
     );
     const span = getByRole('img') as HTMLElement;
     expect(span.className.trim()).toEqual('bi icon bi-snow3');
+    expect(span.getAttribute('aria-label')).toEqual('snow3 icon');
+  });
+
+  test("can use a signal for classname", () => {
+    const className = signal("my-class-name");
+    const { getByRole } = renderWithProviders(
+      <Icon name="step-forward" className={className} />
+    );
+    const span = getByRole('img') as HTMLElement;
+    expect(span.className.trim()).toEqual('bi icon bi-step-forward my-class-name');
   });
 });
