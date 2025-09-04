e74d1899a7306dca329141a828a1788e709dcf4f
Alex Ashley <github@ashley-family.net>
2025-09-04 11:49:51 +0100

    profile_compatibility_flags is an int, not BitArray

--- dashlive/mpeg/codec_strings.py
+++ dashlive/mpeg/codec_strings.py
@@ -99,7 +99,7 @@ class H265Codec(CodecData):
     profile_space: int
     level_idc: int
     tier_flag: int
-    profile_compatibility_flags: bitstring.BitArray
+    profile_compatibility_flags: int
     constraint_indicator_flags: int
 
     def __post_init__(self) -> None:
@@ -135,18 +135,19 @@ class H265Codec(CodecData):
         #   containing the general_progressive_source_flag, each encoded as a
         #   hexadecimal number, and the encoding of each byte separated by a
         #   period; trailing bytes that are zero may be omitted.
-        gps = ['', 'A', 'B', 'C'][self.profile_space]
-        tier = '{}{}'.format('LH'[self.tier_flag], self.level_idc)
-        parts = [
+        gps: str = ['', 'A', 'B', 'C'][self.profile_space]
+        lh: str = 'LH'[self.tier_flag]
+        tier: str = f'{lh}{self.level_idc}'
+        parts: list[str] = [
             str(self.avc_type),
             f'{gps}{self.profile_idc:d}',
             f'{self.profile_compatibility_flags:x}',
             tier,
         ]
-        gcif = self.constraint_indicator_flags
+        gcif: int = self.constraint_indicator_flags
         pos = 40
         while gcif > 0:
-            mask = 0xFF << pos
+            mask: int = 0xFF << pos
             parts.append(f'{(gcif & mask) >> pos:x}'.upper())
             gcif = gcif & ~mask
             pos -= 8
@@ -159,20 +160,20 @@ class H265Codec(CodecData):
 
     @classmethod
     def from_string(cls, codec_string: str) -> CodecData:
-        parts = codec_string.split('.')
+        parts: list[str] = codec_string.split('.')
         if parts[1][0] >= 'A':
-            profile_space = 1 + parts[1][0] - 'A'
+            profile_space: int = 1 + ord(parts[1][0]) - ord('A')
             profile_idc = int(parts[1][1:], 10)
         else:
             profile_space = 0
             profile_idc = int(parts[1], 10)
         profile_compatibility_flags = int(parts[2], 16)
         level_idc = int(parts[3][1:], 10)
-        tier_flag = 1 if parts[3] == 'H' else 0
-        constraint_indicator_flags = 0
+        tier_flag: int = 1 if parts[3] == 'H' else 0
+        constraint_indicator_flags: int = 0
         if len(parts) > 4:
             constraint_indicator_flags = int(''.join(parts[4:]), 16)
-            shift = 8 * (10 - len(parts))
+            shift: int = 8 * (10 - len(parts))
             constraint_indicator_flags = constraint_indicator_flags << shift
         return H265Codec(
             avc_type=parts[0], profile_space=profile_space,
