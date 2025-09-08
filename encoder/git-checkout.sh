9ac545d4ab5d6bc51b52786a26b36cba360b3f71
Alex Ashley <github@ashley-family.net>
2025-09-08 11:29:42 +0100

    apply local patches when checking out a git repo
    there is an open issue with MP4Box that causes it to seg fault
    when encrypting AAC audio files. This commit allows locally
    created patch files to be applied after checking out a git
    repository.

--- encoder/git-checkout.sh
+++ encoder/git-checkout.sh
@@ -13,6 +13,17 @@ GITSHA="$2"
 echo "Cloning ${REPO} @ ${GITSHA}"
 git clone \
     --config advice.detachedHead=false \
+    --config user.email="noreply@local" \
+    --config user.name="Docker Container" \
     --no-tags \
     ${REPO} || die "Failed to clone $REPO"
 (cd $DIRNAME && git checkout ${GITSHA}) || die "Failed to check out ${GITSHA}"
+(cd $DIRNAME && git switch -c dashlive) || die "Failed to create dashlive branch"
+
+if [ -d patches/${DIRNAME} ]; then
+    for patch in patches/${DIRNAME}/*.patch
+    do
+        am=$(readlink -f ${patch})
+        (cd $DIRNAME && git am ${am}) || die "Failed to apply patch ${patch}"
+    done
+fi
