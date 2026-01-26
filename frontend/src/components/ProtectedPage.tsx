cce31184742e9770c076f157ca0a98b529f3d387
Alex Ashley <github@ashley-family.net>
2026-01-26 10:04:46 +0000

    refactor PermissionDenied component into its own file
    Move the PermissionDenied component from the ProtectedPage.tsx
    file into its own file and add unit tests.

--- frontend/src/components/ProtectedPage.tsx
+++ frontend/src/components/ProtectedPage.tsx
@@ -1,45 +1,11 @@
 import { type ComponentChildren, Fragment } from "preact";
 import { useSignal, useSignalEffect } from "@preact/signals";
-import { useContext, useEffect } from "preact/hooks";
-import { useLocation } from "wouter-preact";
-
-import { uiRouteMap } from "@dashlive/routemap";
+import { useContext } from "preact/hooks";
 
 import { WhoAmIContext } from "../user/hooks/useWhoAmI";
 import { EndpointContext } from "../endpoints";
 import { LoadingSpinner } from "./LoadingSpinner";
-import { ErrorCard } from "./ErrorCard";
-
-function PermissionDenied() {
-  const [, setLocation] = useLocation();
-
-  useEffect(() => {
-    const id = window.setTimeout(() => {
-      setLocation(uiRouteMap.login.url());
-    }, 10_000);
-
-    return () => {
-      window.clearTimeout(id);
-    };
-  });
-
-  return (
-    <ErrorCard
-      id="permission-denied"
-      header="You need to log in to access this page">
-      <p className="fs-3">
-        This page is only available for users who have logged in.
-      </p>
-      <p className="fs-4">
-        Probably a good idea to{" "}
-        <a href={uiRouteMap.login.url()} className="link link-underline-light">
-          go to the login page
-        </a>
-        .
-      </p>
-    </ErrorCard>
-  );
-}
+import { PermissionDenied } from "./PermissionDenied";
 
 export interface ProtectedPageProps {
   children: ComponentChildren;
