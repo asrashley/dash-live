c935fb2b359e9827baad134057d49105348e0cab
Alex Ashley <github@ashley-family.net>
2026-02-23 10:21:08 +0000

    generate short option and CGI option types
    As well as generating type hints for the full option names, generate
    them for the short option and CGI options.

--- dashlive/server/options/generate_types.py
+++ dashlive/server/options/generate_types.py
@@ -12,12 +12,16 @@ from pathlib import Path
 import re
 from typing import Any, ClassVar
 
+from dashlive.drm.system import DrmSystem
+
 from .all_options import ALL_OPTIONS
 from .dash_option import DashOption
 
 @dataclass
 class DashOptionTypeHint:
     name: str
+    short: str
+    cgi: str
     py_type: str
     ts_type: str
     default: InitVar[Any] = None
@@ -45,8 +49,17 @@ class DashOptionTypeHint:
                 py_default = ' = None'
         self.py_default = py_default
 
-    def to_python_hint(self) -> str:
-        return f'{self.name}: {self.py_type}{self.py_default}'
+    def to_python_hint(self, form: str) -> str:
+        """
+        Generates the parameter name, type hint and its default value.
+        :form: one of "name", "short" or "cgi"
+        """
+        assert form in {"name", "short", "cgi"}
+        name: str = getattr(self, form)
+        if form != "name" and "OptionsType" in self.py_type:
+            return f'{name}: {form.title()}{self.py_type}{self.py_default}'
+
+        return f'{name}: {self.py_type}{self.py_default}'
 
 
 class OptionsTypesGenerator:
@@ -110,10 +123,10 @@ class OptionsTypesGenerator:
         "datetime.datetime": "Date",
         "tuple": "unknown",
     }
-    PY_TO_TS_RE = re.compile('(\s*)([A-Za-z.]+)($|,|\s|\[)')  # '|'.join([f"({t})" for t in PYTHON_TO_TS_TYPES.keys()]))
+    PY_TO_TS_RE = re.compile(r'(\s*)([A-Za-z.]+)($|,|\s|\[)')  # '|'.join([f"({t})" for t in PYTHON_TO_TS_TYPES.keys()]))
 
     @staticmethod
-    def python_to_ts(m: re.match) -> str:
+    def python_to_ts(m: re.Match[str]) -> str:
         try:
             ts_type: str = OptionsTypesGenerator.PYTHON_TO_TS_TYPES[m.group(2)]
             return f"{m.group(1)}{ts_type}{m.group(3)}"
@@ -121,7 +134,7 @@ class OptionsTypesGenerator:
             return m.group(0)
 
     @staticmethod
-    def translate_tuple(m: re.match) -> str:
+    def translate_tuple(m: re.Match[str]) -> str:
         fields: str = re.sub(OptionsTypesGenerator.PY_TO_TS_RE, OptionsTypesGenerator.python_to_ts, m.group(1))
         return f"[{fields}]"
 
@@ -160,18 +173,24 @@ class OptionsTypesGenerator:
                     sub_options[name] = sub
                     primary_options.append(DashOptionTypeHint(
                         name=opt.prefix,
+                        short=opt.prefix,
+                        cgi=opt.prefix,
                         default=None,
                         factory=name,
                         py_type=name,
                         ts_type=name))
                 sub.append(DashOptionTypeHint(
                     name=opt.full_name,
+                    short=opt.short_name,
+                    cgi=opt.cgi_name,
                     default=opt.default_value(),
                     py_type=cls.guess_python_type(opt),
                     ts_type=cls.guess_typescript_type(opt)))
             else:
                 primary_options.append(DashOptionTypeHint(
                     name=opt.full_name,
+                    short=opt.short_name,
+                    cgi=opt.cgi_name,
                     default=opt.default_value(),
                     py_type=cls.guess_python_type(opt),
                     ts_type=cls.guess_typescript_type(opt)))
@@ -184,18 +203,26 @@ class OptionsTypesGenerator:
     @classmethod
     def create_options_container_types_files(cls, py_dest: Path, ts_dest: Path) -> None:
         primary_options, sub_options = cls.guess_options_container_types()
+        cls.create_python_types_file(py_dest, primary_options, sub_options)
+        cls.create_typescript_types_file(ts_dest, primary_options, sub_options)
+
+    @classmethod
+    def create_python_types_file(cls,
+                                 py_dest: Path,
+                                 primary_options: list[DashOptionTypeHint],
+                                 sub_options: dict[str, list[DashOptionTypeHint]]) -> None:
         print(f"Creating {py_dest}")
         with py_dest.open('wt', encoding='utf-8') as dest:
             dest.write('# this file is auto-generated, do not edit!\n')
             dest.write('# to re-generate this file, use the command:\n')
             dest.write('# uv run -m dashlive.server.options.generate_types\n\n')
-            dest.write('from typing import Any\n')
+            dest.write('import datetime\n')
             dest.write('from dataclasses import dataclass, field\n')
             dest.write('from .options_group import OptionsGroup\n\n')
             for name, options in sub_options.items():
                 dest.write(f'@dataclass\nclass {name}(OptionsGroup):\n')
                 for opt in options:
-                    dest.write(f'    {opt.to_python_hint()}\n')
+                    dest.write(f'    {opt.to_python_hint("name")}\n')
                 dest.write('\n\n')
             dest.write('SUB_OPTION_PREFIX_MAP: dict[str, type] = {\n')
             for name in sub_options.keys():
@@ -205,21 +232,70 @@ class OptionsTypesGenerator:
             dest.write('}\n\n')
             dest.write('@dataclass\nclass OptionsContainerType(OptionsGroup):\n')
             for opt in primary_options:
-                dest.write(f'    {opt.to_python_hint()}\n')
+                dest.write(f'    {opt.to_python_hint("name")}\n')
+            dest.write('\n\n@dataclass\nclass ShortOptionsContainerType(OptionsGroup):\n')
+            for opt in primary_options:
+                if "OptionsType" in opt.py_type:
+                    for name, options in sub_options.items():
+                        if name not in opt.py_type:
+                            continue
+                        for short_opt in options:
+                            dest.write(f'    {short_opt.to_python_hint("short")}\n')
+                else:
+                    dest.write(f'    {opt.to_python_hint("short")}\n')
+            dest.write('\n\n@dataclass\nclass CgiOptionsContainerType(OptionsGroup):\n')
+            for opt in primary_options:
+                if "OptionsType" in opt.py_type:
+                    for name, options in sub_options.items():
+                        if name not in opt.py_type:
+                            continue
+                        for cgi_opt in options:
+                            dest.write(f'    {cgi_opt.to_python_hint("cgi")}\n')
+                else:
+                    dest.write(f'    {opt.to_python_hint("cgi")}\n')
 
+    @classmethod
+    def create_typescript_types_file(cls,
+                                     ts_dest: Path,
+                                     primary_options: list[DashOptionTypeHint],
+                                     sub_options: dict[str, list[DashOptionTypeHint]]) -> None:
         print(f"Creating {ts_dest}")
         with ts_dest.open('wt', encoding='utf-8') as dest:
             dest.write('// this file is auto-generated, do not edit!\n\n')
             dest.write('// to re-generate this file, use the command:\n')
             dest.write('// uv run -m dashlive.server.options.generate_types\n\n')
+            drm_names: list[str] = [f'"{d.lower()}"' for d in DrmSystem.keys()]
+            dest.write(f'export type DrmSystemType = {" | ".join(drm_names)};\n\n')
             for name, options in sub_options.items():
                 dest.write(f'export type {name} = {{\n')
                 for opt in options:
                     dest.write(f'  {opt.name}: {opt.ts_type};\n')
                 dest.write('}\n\n')
+                dest.write(f'export type Short{name} = {{\n')
+                for opt in options:
+                    dest.write(f'  {opt.short}: {opt.ts_type};\n')
+                dest.write('}\n\n')
             dest.write('export type OptionsContainerType = {\n')
             for opt in primary_options:
                 dest.write(f'  {opt.name}: {opt.ts_type};\n')
+            dest.write('}\n\n')
+            dest.write('export type ShortOptionsContainerType = {\n')
+            for opt in primary_options:
+                if "OptionsType" in opt.ts_type:
+                    dest.write(f'  {opt.short}: Short{opt.ts_type};\n')
+                else:
+                    dest.write(f'  {opt.short}: {opt.ts_type};\n')
+            dest.write('}\n\n')
+            dest.write('export type CgiOptionsContainerType = {\n')
+            for opt in primary_options:
+                if "OptionsType" in opt.ts_type:
+                    for name, options in sub_options.items():
+                        if name not in opt.ts_type:
+                            continue
+                        for cgi_opt in options:
+                            dest.write(f'  {cgi_opt.cgi}: {cgi_opt.ts_type};\n')
+                else:
+                    dest.write(f'  {opt.cgi}: {opt.ts_type};\n')
             dest.write('}\n')
 
 
