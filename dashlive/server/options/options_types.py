1ca680939456bbafa08f95f424d630263b96552a
Alex Ashley <github@ashley-family.net>
2026-02-23 10:21:08 +0000

    generate short option and CGI option types
    As well as generating type hints for the full option names, generate
    them for the short option and CGI options.

--- dashlive/server/options/options_types.py
+++ dashlive/server/options/options_types.py
@@ -2,7 +2,7 @@
 # to re-generate this file, use the command:
 # uv run -m dashlive.server.options.generate_types
 
-from typing import Any
+import datetime
 from dataclasses import dataclass, field
 from .options_group import OptionsGroup
 
@@ -98,3 +98,125 @@ class OptionsContainerType(OptionsGroup):
     videoCorruptionFrameCount: int | None = None
     videoErrors: list[tuple[int, str]] = field(default_factory=list)
     videoPlayer: str | None = field(default="native")
+
+
+@dataclass
+class ShortOptionsContainerType(OptionsGroup):
+    ab: bool = True
+    ac: str = field(default="mp4a")
+    ad: str | None = None
+    ahe: list[tuple[int, str]] = field(default_factory=list)
+    ast: datetime.datetime | str = field(default="year")
+    bug: list[str] = field(default_factory=list)
+    clu: str | None = None
+    dft: int | None = None
+    djVer: str | None = None
+    drm: list[tuple] = field(default_factory=list)
+    evs: list[str] = field(default_factory=list)
+    hfc: int | None = None
+    fpd: bool = False
+    lee: int = 16
+    ma: str | None = None
+    mt: str | None = None
+    mhe: list[tuple[int, str]] = field(default_factory=list)
+    mlu: str | None = None
+    mup: int | None = None
+    md: str = field(default="vod")
+    ntps: list[str] = field(default_factory=list)
+    patch: bool = False
+    pinCoun: int = 0
+    pinDura: int = 200
+    pinInba: bool = True
+    pinInte: int = 1000
+    pinStar: int = 0
+    pinTime: int = 100
+    pinValu: str = field(default="0")
+    pinVers: int = 0
+    plu: str | None = None
+    pff: bool = True
+    pvn: float | None = None
+    sctCoun: int = 0
+    sctDura: int = 200
+    sctInba: bool = True
+    sctInte: int = 1000
+    sctProg: int = 1620
+    sctStar: int = 0
+    sctTime: int = 100
+    sctValu: str | None = None
+    sctVers: int = 0
+    st: bool = False
+    skVer: str | None = None
+    tc: str | None = None
+    the: list[tuple[int, str]] = field(default_factory=list)
+    tl: str | None = None
+    ptxLang: str | None = None
+    tbd: int = 1800
+    uc: int | None = None
+    base: bool = True
+    utc: str | None = None
+    utv: str | None = None
+    vcor: list[str] = field(default_factory=list)
+    vcfc: int | None = None
+    vhe: list[tuple[int, str]] = field(default_factory=list)
+    vp: str | None = field(default="native")
+
+
+@dataclass
+class CgiOptionsContainerType(OptionsGroup):
+    abr: bool = True
+    acodec: str = field(default="mp4a")
+    ad_audio: str | None = None
+    aerr: list[tuple[int, str]] = field(default_factory=list)
+    start: datetime.datetime | str = field(default="year")
+    bugs: list[str] = field(default_factory=list)
+    clearkey__la_url: str | None = None
+    drift: int | None = None
+    dashjs: str | None = None
+    drm: list[tuple] = field(default_factory=list)
+    events: list[str] = field(default_factory=list)
+    failures: int | None = None
+    periodDur: bool = False
+    leeway: int = 16
+    main_audio: str | None = None
+    main_text: str | None = None
+    merr: list[tuple[int, str]] = field(default_factory=list)
+    marlin__la_url: str | None = None
+    mup: int | None = None
+    mode: str = field(default="vod")
+    ntp_servers: list[str] = field(default_factory=list)
+    patch: bool = False
+    ping__count: int = 0
+    ping__duration: int = 200
+    ping__inband: bool = True
+    ping__interval: int = 1000
+    ping__start: int = 0
+    ping__timescale: int = 100
+    ping__value: str = field(default="0")
+    ping__version: int = 0
+    playready__la_url: str | None = None
+    playready__piff: bool = True
+    playready__version: float | None = None
+    scte35__count: int = 0
+    scte35__duration: int = 200
+    scte35__inband: bool = True
+    scte35__interval: int = 1000
+    scte35__program_id: int = 1620
+    scte35__start: int = 0
+    scte35__timescale: int = 100
+    scte35__value: str | None = None
+    scte35__version: int = 0
+    timeline: bool = False
+    shaka: str | None = None
+    tcodec: str | None = None
+    terr: list[tuple[int, str]] = field(default_factory=list)
+    tlang: str | None = None
+    text_pref: str | None = None
+    depth: int = 1800
+    update: int | None = None
+    base: bool = True
+    time: str | None = None
+    time_value: str | None = None
+    vcorrupt: list[str] = field(default_factory=list)
+    frames: int | None = None
+    verr: list[tuple[int, str]] = field(default_factory=list)
+    player: str | None = field(default="native")
